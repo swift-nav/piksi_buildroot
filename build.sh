173bd804cfe6ff9e22c602482009708ea0da0c9e
Gareth McMullin <gareth@blacksphere.co.nz>
2016-07-01 08:15:43 +1200

    Initial import

---
+++ build.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+# Abort on Error
+set -e
+
+export BR2_EXTERNAL=..
+export PING_SLEEP=30s
+export WORKDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+export BUILD_OUTPUT=$WORKDIR/build.out
+
+touch $BUILD_OUTPUT
+
+dump_output() {
+   echo Tailing the last 500 lines of output:
+   tail -500 $BUILD_OUTPUT  
+}
+error_handler() {
+  echo ERROR: An error was encountered with the build.
+  dump_output
+  exit 1
+}
+# If an error occurs, run our error handler to output a tail of the build
+trap 'error_handler' ERR
+
+# Set up a repeating loop to send some output to Travis.
+
+bash -c "while true; do echo \$(date) - building ...; sleep $PING_SLEEP; done" &
+PING_LOOP_PID=$!
+
+git clone --depth=1 git://git.buildroot.net/buildroot -b 2016.02
+pushd buildroot
+make piksiv3_defconfig >> $BUILD_OUTPUT 2>&1
+make >> $BUILD_OUTPUT 2>&1
+popd
+
+# The build finished without returning an error so dump a tail of the output
+dump_output
+
+# nicely terminate the ping output loop
+kill $PING_LOOP_PID
+
