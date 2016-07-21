da6c565278c3a8adbd547e0375bccd198b0de84a
Leith Bade <leith@swiftnav.com>
2016-07-20 17:28:02 -0700

    Use git ssh

--- package/libswiftnav/libswiftnav.mk
+++ package/libswiftnav/libswiftnav.mk
@@ -5,7 +5,8 @@
 ################################################################################
 
 LIBSKYLARK_VERSION = 459cb3bc0fa1d0d4b054e32e3d14cccce3397e72
-LIBSKYLARK_SITE = $(call github,swift-nav,libswiftnav-private,$(FOO_VERSION))
+NAVENGINE_SITE_METHOD = git
+NAVENGINE_SITE = ssh://git@github.com:swift-nav/libswiftnav-private.git
 LIBSKYLARK_REDISTRIBUTE = NO
 LIBSKYLARK_INSTALL_STAGING = YES
 LIBSKYLARK_DEPENDENCIES = host-pkgconf
--- package/navengine/navengine.mk
+++ package/navengine/navengine.mk
@@ -5,7 +5,8 @@
 ################################################################################
 
 NAVENGINE_VERSION = 459cb3bc0fa1d0d4b054e32e3d14cccce3397e72
-NAVENGINE_SITE = $(call github,swift-nav,navengine,$(FOO_VERSION))
+NAVENGINE_SITE_METHOD = git
+NAVENGINE_SITE = ssh://git@github.com:swift-nav/navengine.git
 NAVENGINE_REDISTRIBUTE = NO
 NAVENGINE_DEPENDENCIES = eigen gtest gflags libcurl libsbp libserialport \
                          libswiftnav libskylark host-pkgconf openssl zlib
