da4cca88498442c8fad637a97568f5498a1b5f1f
Gareth McMullin <gareth@blacksphere.co.nz>
2016-08-03 07:53:43 +1200

    Update libsbp

--- package/libsbp/libsbp.mk
+++ package/libsbp/libsbp.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-LIBSBP_VERSION = v1.0.0
+LIBSBP_VERSION = v1.1.0
 LIBSBP_SITE = https://github.com/swift-nav/libsbp
 LIBSBP_SITE_METHOD = git
 LIBSBP_INSTALL_STAGING = YES
