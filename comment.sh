f71d1a0dcbe20e19d4de49744a9d739498f2e5af
Mark Fine <mark.fine@gmail.com>
2016-09-13 17:39:22 -0700

    Only publish and notify on master pushes.

--- comment.sh
+++ comment.sh
@@ -26,11 +26,13 @@ BUILD_VERSION="$(git describe --tags --dirty --always)"
 BUILD_PATH="$REPO/$BUILD_VERSION"
 
 if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
-    COMMENT="$BUILD_PATH
+    if [ "$TRAVIS_BRANCH" == "master" ]; then
+        COMMENT="$BUILD_PATH
 https://console.aws.amazon.com/s3/home?region=us-west-2&bucket=swiftnav-artifacts&prefix=$BUILD_PATH/"
-    URL="https://slack.com/api/chat.postMessage?token=$SLACK_TOKEN&channel=$SLACK_CHANNEL"
-    DATA="text=$COMMENT"
-    curl --data-urlencode "$DATA" "$URL"
+        URL="https://slack.com/api/chat.postMessage?token=$SLACK_TOKEN&channel=$SLACK_CHANNEL"
+        DATA="text=$COMMENT"
+        curl --data-urlencode "$DATA" "$URL"
+    fi
 elif [ ! -z "$GITHUB_TOKEN" ]; then
     COMMENT="## $BUILD_VERSION\n+ [s3://$PRS_BUCKET/$BUILD_PATH](https://console.aws.amazon.com/s3/home?region=us-west-2&bucket=swiftnav-artifacts-pull-requests&prefix=$BUILD_PATH/)"
     URL="https://api.github.com/repos/swift-nav/$REPO/issues/$TRAVIS_PULL_REQUEST/comments"
--- publish.sh
+++ publish.sh
@@ -34,8 +34,10 @@ for file in "$@"
 do
     KEY="$BUILD_PATH/$(basename $file)"
     if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
-        OBJECT="s3://$BUCKET/$KEY"
-        aws s3 cp "$file" "$OBJECT"
+        if [ "$TRAVIS_BRANCH" == "master" ]; then
+            OBJECT="s3://$BUCKET/$KEY"
+            aws s3 cp "$file" "$OBJECT"
+        fi
     else
         aws s3api put-object --no-sign-request --bucket "$PRS_BUCKET" --key "$KEY" --body "$file" --acl public-read
     fi
