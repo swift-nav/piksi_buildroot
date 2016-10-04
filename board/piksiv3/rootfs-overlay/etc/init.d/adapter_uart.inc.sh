a42714caa26599cac196b60c73757957fa80e616
Gareth McMullin <gareth@blacksphere.co.nz>
2016-10-04 16:28:13 -0700

    Start zmq_adapter of serial ports.

---
+++ board/piksiv3/rootfs-overlay/etc/init.d/adapter_uart.inc.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+name="zmq_adapter_uart_$ttydev"
+cmd="zmq_adapter --file /dev/$ttydev -p >tcp://127.0.0.1:43031 -s >tcp://127.0.0.1:43030 -f sbp"
+dir="/"
+user=""
+
+case "$1" in
+  start)
+    /bin/stty -F /dev/$ttydev $baudrate raw -echo 
+    ;;
+esac
+
+source /etc/init.d/template_process.inc.sh
+
