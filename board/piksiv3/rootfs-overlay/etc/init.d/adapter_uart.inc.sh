3203bdc6f98e6499f5f369e38f9d0553834d1dad
Jacob McNamee <jacob@swiftnav.com>
2016-10-19 15:48:24 -0700

    Add output filter to UART0

--- board/piksiv3/rootfs-overlay/etc/init.d/adapter_uart.inc.sh
+++ board/piksiv3/rootfs-overlay/etc/init.d/adapter_uart.inc.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 name="zmq_adapter_uart_$ttydev"
-cmd="zmq_adapter --file /dev/$ttydev -p >tcp://127.0.0.1:43031 -s >tcp://127.0.0.1:43030 -f sbp"
+cmd="zmq_adapter --file /dev/$ttydev -p >tcp://127.0.0.1:43031 -s >tcp://127.0.0.1:43030 -f sbp $filter_options"
 dir="/"
 user=""
 
