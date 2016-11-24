dbbdd504fadb12d6ae695cea443728f6e001dc25
Fergus Noble <fergusnoble@gmail.com>
2016-11-23 17:37:14 -0800

    Install firmware files into system image

--- board/piksiv3/post_image.sh
+++ board/piksiv3/post_image.sh
@@ -12,6 +12,7 @@ generate_dev() {
   cp $UBOOT_BUILD_DIR/spl/boot.bin $OUTPUT_DIR
   cp $UBOOT_BUILD_DIR/u-boot.img $OUTPUT_DIR
   cp $BINARIES_DIR/uImage.${CFG} $OUTPUT_DIR
+  cp $BINARIES_DIR/rootfs.cpio $OUTPUT_DIR
 }
 
 generate_prod() {
@@ -23,19 +24,23 @@ generate_prod() {
 
   mkdir -p $OUTPUT_DIR
   cp $UBOOT_BUILD_DIR/tpl/boot.bin $OUTPUT_DIR
+  cp $BINARIES_DIR/rootfs.cpio $OUTPUT_DIR
 
   $UBOOT_BUILD_DIR/tools/image_table_util                                     \
   --append --print --print-images                                             \
   --out $OUTPUT_DIR/image_set.bin                                             \
   --name "Piksi Buildroot $BR_GIT_VERSION"                                    \
   --timestamp $(date +%s)                                                     \
-  --hardware v3_$HW                                                              \
+  --hardware v3_$HW                                                           \
   --image $UBOOT_BUILD_DIR/spl/u-boot-spl-dtb.img --image-type uboot-spl      \
   --image $UBOOT_BUILD_DIR/u-boot.img --image-type uboot                      \
   --image $BINARIES_DIR/uImage.$CFG --image-type linux
 }
 
-generate_dev "microzed"
-generate_prod "microzed"
-generate_dev "prod"
-generate_prod "prod"
+generate_dev $HW_CONFIG
+generate_prod $HW_CONFIG
+
+# Images for this HW_CONFIG have been moved into their relavant output folder,
+# remove the copies from the root of BINARIES_DIR
+rm -f $BINARIES_DIR/uImage.*
+rm -f $BINARIES_DIR/rootfs.cpio
