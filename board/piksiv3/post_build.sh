3d4271256cd8d6b668fec6b43a27251540e8813a
Fergus Noble <fergusnoble@gmail.com>
2016-12-01 12:15:41 -0800

    Remove any existing firmware files from rootfs

--- board/piksiv3/post_build.sh
+++ board/piksiv3/post_build.sh
@@ -10,8 +10,13 @@ FIRMWARE_DIR=$BASE_DIR/../../firmware
 # Create firmware directory in the rootfs
 mkdir -p $FIRMWARE_DIR_ROOTFS
 
+# Remove any existing firmware files
+rm -f $FIRMWARE_DIR_ROOTFS/piksi_firmware.elf
+rm -f $FIRMWARE_DIR_ROOTFS/piksi_fpga.bit
+
 # Install the piksi_firmware and piksi_fpga images into the rootfs
-if [ -e $FIRMWARE_DIR/$HW_CONFIG/piksi_firmware.elf && -e $FIRMWARE_DIR/$HW_CONFIG/piksi_fpga.bit ]; then
+if [ -e $FIRMWARE_DIR/$HW_CONFIG/piksi_firmware.elf && \
+     -e $FIRMWARE_DIR/$HW_CONFIG/piksi_fpga.bit ]; then
   cp $FIRMWARE_DIR/$HW_CONFIG/piksi_firmware.elf $FIRMWARE_DIR_ROOTFS/piksi_firmware.elf
   cp $FIRMWARE_DIR/$HW_CONFIG/piksi_fpga.bit $FIRMWARE_DIR_ROOTFS/piksi_fpga.bit
 else
