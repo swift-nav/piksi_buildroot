1d6caa64bf44361d54a2fad217f2354d9e44a697
Fergus Noble <fergusnoble@gmail.com>
2017-01-06 02:28:36 +0000

    Don’t start UART zmq_adapters from init.d
    As they are now started by piksi_system_daemon

--- board/piksiv3/rootfs-overlay/etc/init.d/adapter_uart.inc.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-name="zmq_adapter_uart_$ttydev"
-cmd="zmq_adapter --file /dev/$ttydev -p >tcp://127.0.0.1:43031 -s >tcp://127.0.0.1:43030 -f sbp $filter_options"
-dir="/"
-user=""
-
-#source /etc/init.d/template_process.inc.sh
-
