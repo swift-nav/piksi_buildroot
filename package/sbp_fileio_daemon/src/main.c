534589953a0b58f72999691d203a0d8292e8cafb
Jacob McNamee <jacob@swiftnav.com>
2017-01-13 18:31:20 -0800

    Launch two sbp_fileio_daemon instances for firmware and external use

--- package/sbp_fileio_daemon/src/main.c
+++ package/sbp_fileio_daemon/src/main.c
@@ -11,8 +11,61 @@
  */
 
 #include <sbp_zmq.h>
+#include <getopt.h>
 
 #include "sbp_fileio.h"
+
+static const char *pub_endpoint = NULL;
+static const char *sub_endpoint = NULL;
+
+static void usage(char *command)
+{
+  printf("Usage: %s\n", command);
+
+  puts("-p, --pub <addr>");
+  puts("-s, --sub <addr>");
+}
+
+static int parse_options(int argc, char *argv[])
+{
+  const struct option long_opts[] = {
+    {"pub", required_argument, 0, 'p'},
+    {"sub", required_argument, 0, 's'},
+    {0, 0, 0, 0}
+  };
+
+  int c;
+  int opt_index;
+  while ((c = getopt_long(argc, argv, "p:s:",
+                          long_opts, &opt_index)) != -1) {
+    switch (c) {
+
+      case 'p': {
+        pub_endpoint = optarg;
+      }
+      break;
+
+      case 's': {
+        sub_endpoint = optarg;
+      }
+      break;
+
+
+      default: {
+        printf("invalid option\n");
+        return -1;
+      }
+      break;
+    }
+  }
+
+  if ((pub_endpoint == NULL) || (sub_endpoint == NULL)) {
+    printf("ZMQ endpoints not specified\n");
+    return -1;
+  }
+
+  return 0;
+}
 
 static int file_read_string(const char *filename, char *str, size_t str_size)
 {
@@ -34,8 +87,13 @@ static int file_read_string(const char *filename, char *str, size_t str_size)
   return 0;
 }
 
-int main(void)
+int main(int argc, char *argv[])
 {
+  if (parse_options(argc, argv) != 0) {
+    usage(argv[0]);
+    exit(1);
+  }
+
   /* Set up SBP ZMQ */
   u16 sbp_sender_id = SBP_SENDER_ID;
   char sbp_sender_id_string[32];
@@ -45,8 +103,8 @@ int main(void)
   }
   sbp_zmq_config_t sbp_zmq_config = {
     .sbp_sender_id = sbp_sender_id,
-    .pub_endpoint = ">tcp://localhost:43021",
-    .sub_endpoint = ">tcp://localhost:43020"
+    .pub_endpoint = pub_endpoint,
+    .sub_endpoint = sub_endpoint
   };
 
   sbp_zmq_state_t sbp_zmq_state;
