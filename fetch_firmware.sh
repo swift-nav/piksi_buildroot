840888f86dbd8f1d063e58faa1b128b87a111ddf
Ben Segal <ben@swift-nav.com>
2017-01-17 16:16:56 -0800

    Split filters.

--- fetch_firmware.sh
+++ fetch_firmware.sh
@@ -15,10 +15,10 @@
 
 set -xe
 
-FW_VERSION=${1:-v1.0.0-branch-25-g5cb0ff6}
+FW_VERSION=${1:-v1.0.0-branch-27-gba4bc6f}
 NAP_VERSION=${2:-v1.0.1}
 
-FW_S3_PATH=s3://swiftnav-artifacts/piksi_firmware_private/$FW_VERSION/v3
+FW_S3_PATH=s3://swiftnav-artifacts-pull-requests/piksi_firmware_private/$FW_VERSION/v3
 NAP_S3_PATH=s3://swiftnav-artifacts/piksi_fpga/$NAP_VERSION
 export AWS_DEFAULT_REGION="us-west-2"
 
@@ -45,4 +45,3 @@ download_fw() {
 
 download_fw "prod"
 download_fw "microzed"
-
