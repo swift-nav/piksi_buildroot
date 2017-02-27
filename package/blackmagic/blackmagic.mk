6a8756da810fb62964b02b4ba0574812a6f095fe
Gareth McMullin <gareth@blacksphere.co.nz>
2017-02-27 15:06:49 +1300

    Add blackmagic zynq_amp package.
    Supports remote debugging of firmware on TCP 2001
    Support core dump of firmware when sent SIGUSR1

---
+++ package/blackmagic/blackmagic.mk
@@ -0,0 +1,19 @@
+################################################################################
+#
+# blackmagic
+#
+################################################################################
+
+BLACKMAGIC_SITE = https://github.com/swift-nav/blackmagic
+BLACKMAGIC_VERSION = c0500b2abe29bc62ebe7709eb7c6a46447e9cfdb
+BLACKMAGIC_SITE_METHOD = git
+
+define BLACKMAGIC_BUILD_CMDS
+    $(MAKE) CC=$(TARGET_CC) LD=$(TARGET_LD) -C $(@D)/src
+endef
+
+define BLACKMAGIC_INSTALL_TARGET_CMDS
+    $(INSTALL) -D -m 0755 $(@D)/src/blackmagic $(TARGET_DIR)/usr/bin
+endef
+
+$(eval $(generic-package))
