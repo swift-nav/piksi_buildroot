c57f108331a188b61cf72316281a6333a833f17f
Mark Fine <mark.fine@gmail.com>
2017-03-08 23:18:19 -0800

    Should be working binaries.

--- package/skylark_download_daemon/src/main.c
+++ package/skylark_download_daemon/src/main.c
@@ -10,9 +10,85 @@
  * WARRANTIES OF MERCHANTABILITY AND/OR FITNESS FOR A PARTICULAR PURPOSE.
  */
 
+#include <stdlib.h>
+#include <unistd.h>
+#include <curl/curl.h>
 #include <sbp_zmq.h>
+
+static size_t callback(void *p, size_t size, size_t n, void *up)
+{
+  ssize_t m = write((int)up, p, size*n);
+  return m;
+}
+
+static void download(int fd)
+{
+  CURLcode res = curl_global_init(CURL_GLOBAL_ALL);
+  if (res != CURLE_OK) {
+    exit(EXIT_FAILURE);
+  }
+
+  CURL *curl = curl_easy_init();
+  if (curl == NULL) {
+    curl_global_cleanup();
+    exit(EXIT_FAILURE);
+  }
+
+  curl_easy_setopt(curl, CURLOPT_URL, "http://localhost:3000");
+  curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, callback);
+  curl_easy_setopt(curl, CURLOPT_WRITEDATA, fd);
+
+  struct curl_slist *chunk = NULL;
+  chunk = curl_slist_append(chunk, "Transfer-Encoding: chunked");
+  chunk = curl_slist_append(chunk, "Accept: application/vnd.swiftnav.broker.v1+sbp2");
+  chunk = curl_slist_append(chunk, "Device-Uid: 22222222-2222-2222-2222-222222222222");
+  curl_easy_setopt(curl, CURLOPT_HTTPHEADER, chunk);
+
+  res = curl_easy_perform(curl);
+  if (res != CURLE_OK) {
+    curl_easy_cleanup(curl);
+    curl_global_cleanup();
+    exit(EXIT_FAILURE);
+  }
+
+  curl_easy_cleanup(curl);
+  curl_global_cleanup();
+}
+
+static void sink(int fd)
+{
+  for (;;) {
+    char buf[1024];
+    ssize_t n = read(fd, buf, sizeof(buf));
+    if (n <= 0) {
+      break;
+    }
+    ssize_t m = fwrite(buf, 1, n, stdout);
+    if (m < n) {
+      break;
+    }
+    fflush(stdout);
+  }
+}
 
 int main(int argc, char *argv[])
 {
+  int fds[2], ret = pipe(fds);
+  if (ret < 0) {
+    exit(EXIT_FAILURE);
+  }
+
+  ret = fork();
+  if (ret < 0) {
+    exit(EXIT_FAILURE);
+  } else if (ret == 0) {
+    close(fds[0]);
+    download(fds[1]);
+  } else {
+    close(fds[1]);
+    sink(fds[0]);
+    waitpid(ret, &ret, 0);
+  }
+
   exit(EXIT_SUCCESS);
 }
--- package/skylark_upload_daemon/src/main.c
+++ package/skylark_upload_daemon/src/main.c
@@ -10,9 +10,90 @@
  * WARRANTIES OF MERCHANTABILITY AND/OR FITNESS FOR A PARTICULAR PURPOSE.
  */
 
+#include <stdlib.h>
+#include <unistd.h>
+#include <curl/curl.h>
+
 #include <sbp_zmq.h>
 
-int main(int argc, char *argv[])
+static size_t callback(void *p, size_t size, size_t n, void *up)
 {
+  ssize_t m = read((int)up, p, size*n);
+  if (m < 0) {
+    return CURL_READFUNC_ABORT;
+  }
+  return m;
+}
+
+static void upload(int fd)
+{
+  CURLcode res = curl_global_init(CURL_GLOBAL_ALL);
+  if (res != CURLE_OK) {
+    exit(EXIT_FAILURE);
+  }
+
+  CURL *curl = curl_easy_init();
+  if (curl == NULL) {
+    curl_global_cleanup();
+    exit(EXIT_FAILURE);
+  }
+
+  curl_easy_setopt(curl, CURLOPT_URL, "http://localhost:3000");
+  curl_easy_setopt(curl, CURLOPT_PUT, 1L);
+  curl_easy_setopt(curl, CURLOPT_READFUNCTION, callback);
+  curl_easy_setopt(curl, CURLOPT_READDATA, fd);
+
+  struct curl_slist *chunk = NULL;
+  chunk = curl_slist_append(chunk, "Transfer-Encoding: chunked");
+  chunk = curl_slist_append(chunk, "Content-Type: application/vnd.swiftnav.broker.v1+sbp2");
+  chunk = curl_slist_append(chunk, "Device-Uid: 22222222-2222-2222-2222-222222222222");
+  curl_easy_setopt(curl, CURLOPT_HTTPHEADER, chunk);
+
+  res = curl_easy_perform(curl);
+  if (res != CURLE_OK) {
+    curl_easy_cleanup(curl);
+    curl_global_cleanup();
+    exit(EXIT_FAILURE);
+  }
+
+  curl_easy_cleanup(curl);
+  curl_global_cleanup();
+}
+
+static void source(int fd)
+{
+  for (;;) {
+    char buf[1024];
+    ssize_t n = fread(buf, 1, sizeof(buf), stdin);
+    if (n <= 0) {
+      break;
+    }
+    ssize_t m = write(fd, buf, n);
+    if (m < n) {
+      break;
+    }
+  }
+}
+
+int main(int argc, char **argv)
+{
+  int fds[2], ret = pipe(fds);
+  if (ret < 0) {
+    exit(EXIT_FAILURE);
+  }
+
+  ret = fork();
+  if (ret < 0) {
+    exit(EXIT_FAILURE);
+  } else if (ret == 0) {
+    close(fds[1]);
+    upload(fds[0]);
+  } else {
+    close(fds[0]);
+    source(fds[1]);
+    waitpid(ret, &ret, 0);
+  }
+
   exit(EXIT_SUCCESS);
 }
+
