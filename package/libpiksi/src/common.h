a188d7e9080e6f67ccbeca421e3947145167e439
Jacob McNamee <jacob@swiftnav.com>
2017-03-17 15:43:04 -0700

    Initial libpiksi implementation

---
+++ package/libpiksi/src/common.h
@@ -0,0 +1,20 @@
+/*
+ * Copyright (C) 2017 Swift Navigation Inc.
+ * Contact: Jacob McNamee <jacob@swiftnav.com>
+ *
+ * This source is subject to the license found in the file 'LICENSE' which must
+ * be be distributed together with this source. All other rights reserved.
+ *
+ * THIS CODE AND INFORMATION IS PROVIDED "AS IS" WITHOUT WARRANTY OF ANY KIND,
+ * EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND/OR FITNESS FOR A PARTICULAR PURPOSE.
+ */
+
+#ifndef LIBPIKSI_COMMON_H
+#define LIBPIKSI_COMMON_H
+
+#include <stdint.h>
+#include <libsbp/sbp.h>
+#include <czmq.h>
+
+#endif /* LIBPIKSI_COMMON_H */
