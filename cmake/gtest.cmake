7a54a4af44172410e8238897bd34e80c0030ec3a
Bhaskar Mookerji <mookerji@gmail.com>
2017-03-21 16:22:03 -0700

    Google test external project.

--- cmake/gtest.cmake
+++ cmake/gtest.cmake
@@ -1,24 +1,39 @@
 cmake_minimum_required(VERSION 2.8)
 
-# This brings in the external project support in cmake
-include(ExternalProject)
+########################### GTEST
+# Enable ExternalProject CMake module
+INCLUDE(ExternalProject)
 
-# build directory
-set(GTEST_PREFIX ${CMAKE_BINARY_DIR}/external/GTEST-prefix)
-# install directory
-set(GTEST_INSTALL ${CMAKE_BINARY_DIR}/external/GTEST-install)
-# This adds zmq as an external project with the specified parameters.
-ExternalProject_Add(googletest-distribution
-  PREFIX ${GTEST_PREFIX}
-  # We use SOURCE_DIR because we use version control to track the
-  # version of this library instead of using the build tool
-  SOURCE_DIR ${CMAKE_SOURCE_DIR}/package/third_party/googletest
-  INSTALL_DIR ${GTEST_INSTALL}
-  #INSTALL_COMMAND cmake -E echo "Not installing ZeroMQ globally."
-  CMAKE_ARGS -DCMAKE_INSTALL_PREFIX=${GTEST_INSTALL}
-  # This simply passes down cmake arguments, which allows us to define
-  # zmq-specific cmake flags as arguments to the toplevel cmake
-  # invocation.
-  CMAKE_ARGS ${CMAKE_ARGS})
-set(GTEST_INCLUDE_DIRS ${GTEST_INSTALL}/include)
-set(GTEST_LIBRARY_DIRS ${GTEST_INSTALL}/lib)
+# Set default ExternalProject root directory
+SET_DIRECTORY_PROPERTIES(PROPERTIES EP_PREFIX ${CMAKE_BINARY_DIR}/third_party)
+
+# Add gtest
+# http://stackoverflow.com/questions/9689183/cmake-googletest
+ExternalProject_Add(
+    googletest
+    URL https://github.com/google/googletest/archive/release-1.8.0.zip
+    TIMEOUT 10
+    # # Force separate output paths for debug and release builds to allow easy
+    # # identification of correct lib in subsequent TARGET_LINK_LIBRARIES
+    # # commands # CMAKE_ARGS
+    # # -DCMAKE_ARCHIVE_OUTPUT_DIRECTORY_DEBUG:PATH=DebugLibs #
+    # # -DCMAKE_ARCHIVE_OUTPUT_DIRECTORY_RELEASE:PATH=ReleaseLibs #
+    # # -Dgtest_force_shared_crt=ON # Disable install step
+    INSTALL_COMMAND ""
+    # Wrap download, configure and build steps in a script to log output
+    LOG_DOWNLOAD ON
+    LOG_CONFIGURE ON
+    LOG_BUILD ON)
+
+# Specify include dir
+ExternalProject_Get_Property(googletest source_dir)
+set(GTEST_INCLUDE_DIR ${source_dir}/include)
+
+# Library
+ExternalProject_Get_Property(googletest binary_dir)
+set(GTEST_LIBRARY_PATH ${binary_dir}/${CMAKE_FIND_LIBRARY_PREFIXES}gtest.a)
+set(GTEST_LIBRARY gtest)
+add_library(${GTEST_LIBRARY} UNKNOWN IMPORTED)
+set_property(TARGET ${GTEST_LIBRARY} PROPERTY IMPORTED_LOCATION
+                ${GTEST_LIBRARY_PATH} )
+add_dependencies(${GTEST_LIBRARY} googletest)
