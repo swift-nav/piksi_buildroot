57b65b3e23e5f86071f4082687dddb5c27e5db69
jdiamond <jonathan@swiftnav.com>
2017-03-21 17:33:20 -0700

    Set external libs to match buildroot versions. For libsbp pull version from mk file and tell cmake that cmakelist is in c dir

--- cmake/zmq.cmake
+++ cmake/zmq.cmake
@@ -10,9 +10,10 @@ set(ZMQ_INSTALL ${CMAKE_BINARY_DIR}/external/ZMQ-install)
 # This adds zmq as an external project with the specified parameters.
 ExternalProject_Add(ZeroMQ
   PREFIX ${ZMQ_PREFIX}
-  # We use SOURCE_DIR because we use version control to track the
-  # version of this library instead of using the build tool
-  SOURCE_DIR ${CMAKE_SOURCE_DIR}/package/third_party/libzmq
+  # checks out version that matches buildroot/package/zeromq/zeromq.mk
+  # release download doesn't build in cmake so using repo
+  GIT_REPOSITORY https://github.com/zeromq/zeromq4-1
+  GIT_TAG v4.1.5
   INSTALL_DIR ${ZMQ_INSTALL}
   #INSTALL_COMMAND cmake -E echo "Not installing ZeroMQ globally."
   CMAKE_ARGS -DZMQ_BUILD_TESTS=OFF -DCMAKE_INSTALL_PREFIX=${ZMQ_INSTALL}
@@ -30,9 +31,9 @@ set(CZMQ_INSTALL ${CMAKE_BINARY_DIR}/external/CZMQ-install)
 # This adds czmq as an external project with the specified parameters.
 ExternalProject_Add(CZeroMQ
   PREFIX ${CZMQ_PREFIX}
-  # We use SOURCE_DIR because we use version control to track the
-  # version of this library instead of using the build tool
-  SOURCE_DIR ${CMAKE_SOURCE_DIR}/package/third_party/czmq
+  #checks out version that matches buildroot/package/czmq/czmq.mk
+  GIT_REPOSITORY https://github.com/zeromq/czmq
+  GIT_TAG 5205ec201e97c3a652c17eb86b18b70350b54512
   INSTALL_DIR ${CZMQ_INSTALL}
   # Pass the ZMQ library and header locations to czmq's FindLibZMQ cmake
   # file. It seems that CZMQ on some Linux systems (i.e., Travis) ignore
