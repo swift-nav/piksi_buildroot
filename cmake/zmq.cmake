5b5b555a58e53530c22d8c792d78f684dc6c7fbd
jdiamond <jonathan@swiftnav.com>
2017-03-22 20:24:28 -0700

    made external library imports consistent with cmake recomended pattern

--- cmake/zmq.cmake
+++ cmake/zmq.cmake
@@ -22,7 +22,22 @@ ExternalProject_Add(ZeroMQ
   # invocation.
   CMAKE_ARGS ${CMAKE_ARGS})
 set(LIBZMQ_INCLUDE_DIRS ${ZMQ_INSTALL}/include)
-set(LIBZMQ_LIBRARY_DIRS ${ZMQ_INSTALL}/lib)
+set(LIBZMQ_LIBRARY_DIR ${ZMQ_INSTALL}/lib)
+
+set(ZMQ_LIBRARY_PATH ${LIBZMQ_LIBRARY_DIR}/${CMAKE_FIND_LIBRARY_PREFIXES}zmq${CMAKE_SHARED_LIBRARY_SUFFIX})
+set(ZMQ_LIBRARY zmq)
+add_library(${ZMQ_LIBRARY} SHARED IMPORTED)
+set_property(TARGET ${ZMQ_LIBRARY} PROPERTY IMPORTED_LOCATION
+                ${ZMQ_LIBRARY_PATH} )
+add_dependencies(${ZMQ_LIBRARY} ZeroMQ)
+
+set(ZMQ_STATIC_LIBRARY_PATH ${LIBZMQ_LIBRARY_DIR}/${CMAKE_FIND_LIBRARY_PREFIXES}zmq-static${CMAKE_STATIC_LIBRARY_SUFFIX})
+set(ZMQ_STATIC_LIBRARY zmq-static)
+add_library(${ZMQ_STATIC_LIBRARY} STATIC IMPORTED)
+set_property(TARGET ${ZMQ_STATIC_LIBRARY} PROPERTY IMPORTED_LOCATION
+                ${ZMQ_STATIC_LIBRARY_PATH} )
+add_dependencies(${ZMQ_STATIC_LIBRARY} ZeroMQ)
+
 
 # build directory
 set(CZMQ_PREFIX ${CMAKE_BINARY_DIR}/external/CZMQ-prefix)
@@ -40,12 +55,19 @@ ExternalProject_Add(CZeroMQ
   # explicit paths and rely on HINTS to find a project-local libzmq. Developer
   # machines are exactly the opposite. For the moment, we pass both paths here
   # to satisfy integration requirements on Travis and local use.
-  CMAKE_ARGS -DPC_LIBZMQ_INCLUDE_DIRS=${LIBZMQ_INCLUDE_DIRS} -DCMAKE_INSTALL_PREFIX=${CZMQ_INSTALL} -DPC_LIBZMQ_LIBRARY_DIRS=${LIBZMQ_LIBRARY_DIRS} -DPC_LIBZMQ_INCLUDE_HINTS=${LIBZMQ_INCLUDE_DIRS} -DPC_LIBZMQ_LIBRARY_HINTS=${LIBZMQ_LIBRARY_DIRS}
+  CMAKE_ARGS -DLIBZMQ_INCLUDE_DIRS=${LIBZMQ_INCLUDE_DIRS} -DCMAKE_INSTALL_PREFIX=${CZMQ_INSTALL} -DLIBZMQ_LIBRARIES=${ZMQ_LIBRARY_PATH}
   # This simply passes down cmake arguments, which allows us to define
   # zmq-specific cmake flags as arguments to the toplevel cmake
   # invocation.
   CMAKE_ARGS ${CMAKE_ARGS})
 
 set(CZMQ_INCLUDE_DIRS ${CZMQ_INSTALL}/include ${LIBZMQ_INCLUDE_DIRS})
-set(CZMQ_LIBRARY_DIRS ${CZMQ_INSTALL}/lib ${LIBZMQ_LIBRARY_DIRS})
+set(CZMQ_LIBRARY_DIR ${CZMQ_INSTALL}/lib)
 add_dependencies(CZeroMQ ZeroMQ)
+
+set(CZMQ_LIBRARY_PATH ${CZMQ_LIBRARY_DIR}/${CMAKE_FIND_LIBRARY_PREFIXES}czmq${CMAKE_SHARED_LIBRARY_SUFFIX})
+set(CZMQ_LIBRARY czmq)
+add_library(${CZMQ_LIBRARY} SHARED IMPORTED)
+set_property(TARGET ${CZMQ_LIBRARY} PROPERTY IMPORTED_LOCATION
+                ${CZMQ_LIBRARY_PATH} )
+add_dependencies(${CZMQ_LIBRARY} CZeroMQ)
