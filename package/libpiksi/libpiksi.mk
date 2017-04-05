754b11356583f3b3482bc3a84e0008f1b4f3c1ea
Jacob McNamee <jacob@swiftnav.com>
2017-04-05 01:47:23 -0700

    Fix directory permissions for /usr/include/libpiksi

--- package/libpiksi/libpiksi.mk
+++ package/libpiksi/libpiksi.mk
@@ -17,7 +17,7 @@ endef
 define LIBPIKSI_INSTALL_STAGING_CMDS
     $(INSTALL) -D -m 0755 $(@D)/libpiksi.so* $(STAGING_DIR)/usr/lib
     $(INSTALL) -D -m 0755 $(@D)/libpiksi.a $(STAGING_DIR)/usr/lib
-    $(INSTALL) -d -m 0644 $(STAGING_DIR)/usr/include/libpiksi
+    $(INSTALL) -d -m 0755 $(STAGING_DIR)/usr/include/libpiksi
     $(INSTALL) -D -m 0644 $(@D)/*.h $(STAGING_DIR)/usr/include/libpiksi
 endef
 
