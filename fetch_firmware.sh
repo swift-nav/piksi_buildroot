fdc258dde06bd0c7f9177ef3ee21e59cf0ea2a96
Colin Beighley <colinbeighley@gmail.com>
2017-04-14 18:16:03 -0700

    Release fetch_firmware.sh

--- fetch_firmware.sh
+++ fetch_firmware.sh
@@ -15,8 +15,8 @@
 
 set -xe
 
-FW_VERSION=${1:-v1.1.5}
-NAP_VERSION=${2:-v1.1.5}
+FW_VERSION=${1:-v1.1.6}
+NAP_VERSION=${2:-v1.1.6}
 
 FW_S3_PATH=s3://swiftnav-artifacts/piksi_firmware_private/$FW_VERSION/v3
 NAP_S3_PATH=s3://swiftnav-artifacts/piksi_fpga/$NAP_VERSION
