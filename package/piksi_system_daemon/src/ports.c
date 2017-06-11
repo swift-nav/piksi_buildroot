ec0555c7323f7a39455d971e7bb76b075ccba1c5
Gareth McMullin <gareth@blacksphere.co.nz>
2017-06-12 11:06:42 +1200

    piksi_system_daemon: Set queue limits on physical ports.

--- package/piksi_system_daemon/src/ports.c
+++ package/piksi_system_daemon/src/ports.c
@@ -71,7 +71,7 @@ typedef struct {
 static port_config_t port_configs[] = {
   {
     .name = "uart0",
-    .opts = "--file /dev/ttyPS0",
+    .opts = "--file /dev/ttyPS0 --nonblock --outq 4096",
     .opts_get = NULL,
     .type = PORT_TYPE_UART,
     .mode_name_default = MODE_NAME_DEFAULT,
@@ -80,7 +80,7 @@ static port_config_t port_configs[] = {
   },
   {
     .name = "uart1",
-    .opts = "--file /dev/ttyPS1",
+    .opts = "--file /dev/ttyPS1 --nonblock --outq 4096",
     .opts_get = NULL,
     .type = PORT_TYPE_UART,
     .mode_name_default = MODE_NAME_DEFAULT,
@@ -89,7 +89,7 @@ static port_config_t port_configs[] = {
   },
   {
     .name = "usb0",
-    .opts = "--file /dev/ttyGS0",
+    .opts = "--file /dev/ttyGS0 --nonblock --outq 4096",
     .opts_get = NULL,
     .type = PORT_TYPE_USB,
     .mode_name_default = MODE_NAME_DEFAULT,
