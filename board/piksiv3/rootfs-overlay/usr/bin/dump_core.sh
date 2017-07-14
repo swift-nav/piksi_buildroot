9515b211cf1739159fc87082b85fa48f5dc6e10a
Luca Baradel <luca@Lucas-MacBook-Pro.local>
2017-07-14 15:49:36 -0700

    Enabled Linux core dumps and send an sbp message when one occurs

---
+++ board/piksiv3/rootfs-overlay/usr/bin/dump_core.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+/usr/bin/logger "CORE DUMP!! executable=$1 pid=$2 time=$3"
+echo "CORE DUMP!! executable=$1 pid=$2 time=$3" | /usr/bin/sbp_log --error
+
+corefile="/tmp/cores/core-$1-$2-$3"
+touch $corefile
+
+while read line
+do
+  echo "$line" >> $corefile
+done
+
