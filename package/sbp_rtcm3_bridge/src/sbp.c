e8eb3da3e0fb845ebeb5d8d362cfa2b247337ae9
Anthony Cole <anthony@swiftnav.com>
2017-07-20 18:59:44 -0700

    Add sender ID support

--- package/sbp_rtcm3_bridge/src/sbp.c
+++ package/sbp_rtcm3_bridge/src/sbp.c
@@ -30,13 +30,13 @@ int sbp_init(sbp_zmq_rx_ctx_t *rx_ctx, sbp_zmq_tx_ctx_t *tx_ctx)
   return 0;
 }
 
-void sbp_message_send(u8 msg_type, u8 len, u8 *payload)
+void sbp_message_send(u8 msg_type, u8 len, u8 *payload, u16 sender_id)
 {
   if (ctx.tx_ctx == NULL) {
     return;
   }
 
-  sbp_zmq_tx_send(ctx.tx_ctx, msg_type, len, payload);
+  sbp_zmq_tx_send_from(ctx.tx_ctx, msg_type, len, payload, sender_id);
 }
 
 int sbp_callback_register(u16 msg_type, sbp_msg_callback_t cb, void *context)
