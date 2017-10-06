3a627b0ff248e871a1cdd8aaebc19e0b678065f0
Jason Mobarak <jason@swiftnav.com>
2017-10-06 14:31:58 -0700

    Fetch a matched version of PFWP

--- fetch_firmware.sh
+++ fetch_firmware.sh
@@ -15,7 +15,7 @@
 
 set -xe
 
-FW_VERSION=${1:-v1.1.0-develop-2017090721}
+FW_VERSION=${1:-v1.2.0-branch-61-g3db484ad}
 NAP_VERSION=${2:-v1.2.0-branch-16-g4a15e21}
 
 FW_S3_PATH=s3://swiftnav-artifacts/piksi_firmware_private/$FW_VERSION/v3
