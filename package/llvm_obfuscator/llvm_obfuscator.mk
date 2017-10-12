f47fcb0b71fdaece602d6075a4fcad01a7b24170
Jason Mobarak <jason@swiftnav.com>
2017-10-12 15:26:04 -0700

    Upgrade to v2 of llvm_obfuscator

--- package/llvm_obfuscator/llvm_obfuscator.mk
+++ package/llvm_obfuscator/llvm_obfuscator.mk
@@ -4,24 +4,28 @@
 #
 ################################################################################
 
-HOST_LLVM_OBFUSCATOR_VERSION = v1
-HOST_LLVM_OBFUSCATOR_ACTUAL_SOURCE_TARBALL = llvm-obfuscated-arm-$(HOST_LLVM_OBFUSCATOR_VERSION).tgz
-HOST_LLVM_OBFUSCATOR_SOURCE = 5043255
+HOST_LLVM_OBFUSCATOR_VERSION = v2
+HOST_LLVM_OBFUSCATOR_ACTUAL_SOURCE_TARBALL = llvm-obfuscator-arm-$(HOST_LLVM_OBFUSCATOR_VERSION).tar.xz
+HOST_LLVM_OBFUSCATOR_SOURCE = 5051967
 HOST_LLVM_OBFUSCATOR_SITE = https://$(GITHUB_TOKEN):@api.github.com/repos/swift-nav/llvm-obfuscator-arm/releases/assets
 HOST_LLVM_OBFUSCATOR_METHOD = wget
 HOST_LLVM_OBFUSCATOR_DL_OPTS = --auth-no-challenge --header='Accept:application/octet-stream'
+HOST_LLVM_OBFUSCATOR_DEPENDENCIES = host-xz
 
 define HOST_LLVM_OBFUSCATOR_PRE_EXTRACT_FIXUP
 	if ! [ -e $(DL_DIR)/$(HOST_LLVM_OBFUSCATOR_ACTUAL_SOURCE_TARBALL) ]; then \
-		mv -v $(DL_DIR)/5043255 $(DL_DIR)/$(HOST_LLVM_OBFUSCATOR_ACTUAL_SOURCE_TARBALL); fi
+		mv -v $(DL_DIR)/$(HOST_LLVM_OBFUSCATOR_SOURCE) $(DL_DIR)/$(HOST_LLVM_OBFUSCATOR_ACTUAL_SOURCE_TARBALL); fi
 	$(eval HOST_LLVM_OBFUSCATOR_SOURCE=$(HOST_LLVM_OBFUSCATOR_ACTUAL_SOURCE_TARBALL))
 endef
 
 HOST_LLVM_OBFUSCATOR_PRE_EXTRACT_HOOKS += HOST_LLVM_OBFUSCATOR_PRE_EXTRACT_FIXUP
 
-define HOST_LLVM_OBFUSCATOR_BUILD_CMDS
+define HOST_LLVM_OBFUSCATOR_INSTALL_CMDS
 	mkdir -p $(HOST_DIR)/opt/llvm-obfuscator
 	rsync -az $(@D)/opt/llvm-obfuscator/ $(HOST_DIR)/opt/llvm-obfuscator/
 endef
+
+LLVM_OBF_CC = $(HOST_DIR)/opt/llvm-obfuscator/wrappers/bin/arm-linux-gnueabihf-clang
+LLVM_OBF_CXX = $(HOST_DIR)/opt/llvm-obfuscator/wrappers/bin/arm-linux-gnueabihf-clang++
 
 $(eval $(host-generic-package))
