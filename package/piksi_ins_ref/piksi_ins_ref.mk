e102a2ecdf115c0fe72c0d96cf11c2ae98b67d32
Jason Mobarak <jason@swiftnav.com>
2017-10-24 15:25:59 -0700

    Integrate ref. design tests into buildroot

--- package/piksi_ins_ref/piksi_ins_ref.mk
+++ package/piksi_ins_ref/piksi_ins_ref.mk
@@ -4,20 +4,39 @@
 #
 ################################################################################
 
-PIKSI_INS_REF_VERSION = v8
+PIKSI_INS_REF_VERSION = v11
 PIKSI_INS_REF_SITE = git@github.com:swift-nav/piksi_inertial_ipsec.git
 PIKSI_INS_REF_SITE_METHOD = git
 PIKSI_INS_REF_DEPENDENCIES = host-llvm_obfuscator
 
-define PIKSI_INS_REF_BUILD_CMDS
+ifeq    ($(BR2_BUILD_TESTS),y)
+ifeq    ($(BR2_RUN_TESTS),y)
+
+define PIKSI_INS_REF_BUILD_CMDS_TESTS
+	$(MAKE) CC=$(TARGET_CC) CXX=$(TARGET_CXX) AR=ar STRIP=strip HOSTCC=$(TARGET_CC) \
+					HOSTCXX=$(TARGET_CXX) OBJCOPY=objcopy OBF_FLAGS= \
+	-j1 -C $(@D) clean all test
+endef
+
+endif # ($(BR2_RUN_TESTS),y)
+endif # ($(BR2_BUILD_TESTS),y)
+
+ifneq    ($(BR2_BUILD_TESTS),y)
+define PIKSI_INS_REF_BUILD_FOR_TARGET
 	$(MAKE) CC=$(LLVM_OBF_CC) CXX=$(LLVM_OBF_CXX) AR=$(LLVM_OBF_AR) \
 				  STRIP=$(LLVM_OBF_STRIP) HOSTCC=$(LLVM_OBF_HOSTCC) \
 					HOSTCXX=$(LLVM_OBF_HOSTCXX) OBJCOPY=$(LLVM_OBF_OBJCOPY) \
-		-j1 -C $(@D) selfverify
+		-j1 -C $(@D) clean all
+endef
+endif # ($(BR2_BUILD_TESTS),y)
+
+define PIKSI_INS_REF_BUILD_CMDS
+	$(PIKSI_INS_REF_BUILD_FOR_TARGET)
+	$(PIKSI_INS_REF_BUILD_CMDS_TESTS)
 endef
 
 define PIKSI_INS_REF_INSTALL_TARGET_CMDS
-	$(INSTALL) -D -m 0755 $(@D)/selfverify $(TARGET_DIR)/usr/bin
+	$(INSTALL) -D -m 0755 $(@D)/bin/selfverify $(TARGET_DIR)/usr/bin
 endef
 
 $(eval $(generic-package))
