4587c450e21791a252b7bfa1f8b90d439176d0f8
Jason Mobarak <jason@swiftnav.com>
2017-11-03 21:02:30 -0700

    Use CRL repository piksi_ins_ref

--- package/piksi_ins_ref/piksi_ins_ref.mk
+++ package/piksi_ins_ref/piksi_ins_ref.mk
@@ -4,8 +4,8 @@
 #
 ################################################################################
 
-PIKSI_INS_REF_VERSION = v12
-PIKSI_INS_REF_SITE = git@github.com:swift-nav/piksi_inertial_ipsec.git
+PIKSI_INS_REF_VERSION = v1
+PIKSI_INS_REF_SITE = git@github.com:swift-nav/piksi_inertial_ipsec_crl.git
 PIKSI_INS_REF_SITE_METHOD = git
 PIKSI_INS_REF_DEPENDENCIES = host-llvm_obfuscator
 
@@ -13,9 +13,10 @@ ifeq    ($(BR2_BUILD_TESTS),y)
 ifeq    ($(BR2_RUN_TESTS),y)
 
 define PIKSI_INS_REF_BUILD_CMDS_TESTS
+	$(MAKE) -j1 -C $(@D) submodules_as_archives
 	$(MAKE) CC=$(TARGET_CC) CXX=$(TARGET_CXX) AR=ar STRIP=strip HOSTCC=$(TARGET_CC) \
 					HOSTCXX=$(TARGET_CXX) OBJCOPY=objcopy OBF_FLAGS= \
-	-j1 -C $(@D) all test
+		-j1 -C $(@D) all test
 endef
 
 endif # ($(BR2_RUN_TESTS),y)
@@ -23,6 +24,7 @@ endif # ($(BR2_BUILD_TESTS),y)
 
 ifneq    ($(BR2_BUILD_TESTS),y)
 define PIKSI_INS_REF_BUILD_FOR_TARGET
+	$(MAKE) -j1 -C $(@D) submodules_as_archives
 	$(MAKE) CC=$(LLVM_OBF_CC) CXX=$(LLVM_OBF_CXX) AR=$(LLVM_OBF_AR) \
 				  STRIP=$(LLVM_OBF_STRIP) HOSTCC=$(LLVM_OBF_HOSTCC) \
 					HOSTCXX=$(LLVM_OBF_HOSTCXX) OBJCOPY=$(LLVM_OBF_OBJCOPY) \
