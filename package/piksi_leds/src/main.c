11299538be7a376af6369b1549804c51577a7e26
Gareth McMullin <gareth@blacksphere.co.nz>
2017-11-09 09:43:46 +1300

    piksi_leds: Add shell package

---
+++ package/piksi_leds/src/main.c
@@ -0,0 +1,42 @@
+/*
+ * Copyright (C) 2017 Swift Navigation Inc.
+ * Contact: Gareth McMullin <gareth@swiftnav.com>
+ *
+ * This source is subject to the license found in the file 'LICENSE' which must
+ * be be distributed together with this source. All other rights reserved.
+ *
+ * THIS CODE AND INFORMATION IS PROVIDED "AS IS" WITHOUT WARRANTY OF ANY KIND,
+ * EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND/OR FITNESS FOR A PARTICULAR PURPOSE.
+ */
+
+#include <libpiksi/sbp_zmq_pubsub.h>
+#include <libpiksi/logging.h>
+#include <libpiksi/util.h>
+
+#define PROGRAM_NAME "piksi_leds"
+
+#define PUB_ENDPOINT ">tcp://localhost:43031"
+#define SUB_ENDPOINT ">tcp://localhost:43030"
+
+int main(void)
+{
+  logging_init(PROGRAM_NAME);
+
+  /* Prevent czmq from catching signals */
+  zsys_handler_set(NULL);
+
+  sbp_zmq_pubsub_ctx_t *ctx = sbp_zmq_pubsub_create(PUB_ENDPOINT, SUB_ENDPOINT);
+  if (ctx == NULL) {
+    exit(EXIT_FAILURE);
+  }
+
+  /* Probe for Duro host */
+  /* Initialise LED hardware */
+  /* Set up SBP listeners */
+
+  zmq_simple_loop(sbp_zmq_pubsub_zloop_get(ctx));
+
+  sbp_zmq_pubsub_destroy(&ctx);
+  exit(EXIT_SUCCESS);
+}
