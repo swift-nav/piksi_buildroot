41d815171eddc38ffce1cbd8e70a34e21536cf80
Jared Wilson <jared@swiftnav.com>
2017-11-17 11:03:25 -0800

    [TEST ONLY] update FW pointer for VG changes and rebase

--- fetch_firmware.sh
+++ fetch_firmware.sh
@@ -15,7 +15,7 @@
 
 set -xe
 
-FW_VERSION=${1:-v1.2.0-develop-2017110820-14-gbd0ddbc6}
+FW_VERSION=${1:-v1.3.2-4-g274de4b2}
 NAP_VERSION=${2:-v1.2.0-develop-2017110820-2-gbad165e}
 
 FW_S3_PATH=s3://swiftnav-artifacts-pull-requests/piksi_firmware_private/$FW_VERSION/v3
