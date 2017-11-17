85b895ea8659724dceee374799ef6fd39b350e6c
Jason Mobarak <jason@swiftnav.com>
2017-11-16 20:46:05 -0800

    Use inotify to notice when the modem is ready

---
+++ package/piksi_system_daemon/piksi_system_daemon/src/cellmodem_inotify.h
@@ -0,0 +1,28 @@
+/*
+ * Copyright (C) 2017 Swift Navigation Inc.
+ * Contact: Swift Navigation <dev@swiftnav.com>
+ *
+ * This source is subject to the license found in the file 'LICENSE' which must
+ * be be distributed together with this source. All other rights reserved.
+ *
+ * THIS CODE AND INFORMATION IS PROVIDED "AS IS" WITHOUT WARRANTY OF ANY KIND,
+ * EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND/OR FITNESS FOR A PARTICULAR PURPOSE.
+ */
+
+#ifndef _SWIFT_CELLMODEM_INOTIFY_H
+#define _SWIFT_CELLMODEM_INOTIFY_H
+
+typedef struct {
+  sbp_zmq_pubsub_ctx_t *pubsub_ctx;
+  zloop_t *loop;
+  zmq_pollitem_t pollitem;
+  int inotify_fd;
+  int watch_descriptor;
+  char cellmodem_dev[32];
+} inotify_ctx_t;
+
+bool cellmodem_tty_exists(const char* path);
+void async_wait_for_tty(sbp_zmq_pubsub_ctx_t *pubsub_ctx, const char* cellmodem_dev);
+
+#endif//_SWIFT_CELLMODEM_INOTIFY_H
