d4c0ccfb26fea258cd8d928fc9b07024dbfb6dac
Jason Mobarak <jason@swiftnav.com>
2017-11-28 12:34:15 -0800

    Tweak curl options, relax file pattern

--- docker/travis.sh
+++ docker/travis.sh
@@ -8,7 +8,7 @@ D=$( (cd "$(dirname "$0")" || exit 1 >/dev/null; pwd -P) )
 if [[ "$TRAVIS_PULL_REQUEST" != "false" ]]; then
 
   files_url="https://api.github.com/repos/$TRAVIS_REPO_SLUG/pulls/$TRAVIS_PULL_REQUEST/files"
-  file_names=$( curl "$files_url" | jq '.[] | .filename' | tr '"' ' ' )
+  file_names=$( curl -sSL "$files_url" | jq '.[] | .filename' | tr '"' ' ' )
 
 else
   file_names=$( git diff --name-only "$TRAVIS_COMMIT_RANGE" || echo "" )
@@ -19,7 +19,7 @@ echo "$file_names"
 
 echo "..."
 
-if echo "$file_names" | grep -q -E "^.*[/]Dockerfile[.]base *$"; then
+if echo "$file_names" | grep -q -E "/Dockerfile[.]base"; then
 	"$D/base.bash"
 else
   echo "No modifications to base Docker image found..."
