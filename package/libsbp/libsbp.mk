6aaf260bf42a9433141a1ce482823029c9aeb19a
dzollo <dzollo@swift-nav.com>
2017-11-29 10:08:07 -0800

    Update SBP Version for inertial messages

--- package/libsbp/libsbp.mk
+++ package/libsbp/libsbp.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-LIBSBP_VERSION = v2.2.7
+LIBSBP_VERSION = cce8a1e370a456af8a056fbfc42e1e35624c4a4c
 LIBSBP_SITE = https://github.com/swift-nav/libsbp
 LIBSBP_SITE_METHOD = git
 LIBSBP_INSTALL_STAGING = YES
