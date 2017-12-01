c5b5215c4d0b5fe35a277203da469aee0118f3fe
scarcanague <sebastien@swiftnav.com>
2017-11-30 17:23:31 -0800

    Update of whitelist with iono and GLO phase biases

--- package/piksi_system_daemon/piksi_system_daemon/src/whitelists.c
+++ package/piksi_system_daemon/piksi_system_daemon/src/whitelists.c
@@ -59,7 +59,7 @@ static port_whitelist_config_t port_whitelist_config[PORT_MAX] = {
   },
   [PORT_UART1] = {
     .name = "uart1",
-    .wl = "23,29,65,72,74,80,134,136,163,165,166,167,171,181,185,187,188,257,258,259,520,522,524,526,527,528,1025,2304,2305,65280,65282,65535"
+    .wl = "23,29,65,72,74,80,117,134,136,144,163,165,166,167,171,181,185,187,188,257,258,259,520,522,524,526,527,528,1025,2304,2305,65280,65282,65535"
     /*  This filter represents the messages in use by the console.
         It removes all ECEF nav messages as well as parts of nav msg.
         MsgThreadState             23
@@ -68,8 +68,10 @@ static port_whitelist_config_t port_whitelist_config[PORT_MAX] = {
         MsgBasePosECEF             72
         MsgObs                     74
         MsgSpecan                  80
+        MsgGloBiases              117        
         MsgEphemerisGPS           134
         MsgEphemerisGlo           136
+        MsgIono                   144
         MsgFileioReadResp         163
         MsgSettingsReadResp       165
         MsgSettingsReadByIn       166
