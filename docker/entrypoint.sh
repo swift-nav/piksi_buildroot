a4c7c27c583f4bfccc296abe33c9666eeb54cfb6
Jason Mobarak <jason@swiftnav.com>
2017-12-07 11:21:05 -0800

    Fix setup issues with adding an existing group

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
-export USER=$USER
+export USER
+export GID
 
 sudo rm -rf "/home/$USER/.ssh"
 sudo cp -r /host-ssh "/home/$USER/.ssh"
@@ -8,18 +9,18 @@ sudo cp -r /host-ssh "/home/$USER/.ssh"
 sudo chmod 0770 /root
 sudo find /root -type f -exec chmod g+rw {} \;
 
-sudo chown -R "$USER:$USER" "/home/$USER/.ssh"
+sudo chown -R "$USER:$GID" "/home/$USER/.ssh"
 sudo chmod 0700 "/home/$USER/.ssh"
 
 sudo find "/home/$USER/.ssh" -type f -exec chmod 0400 {} \;
 
 [ -d "/piksi_buildroot/buildroot" ] && \
-  sudo chown "$USER:$USER" "/piksi_buildroot/buildroot"
+  sudo chown "$USER:$GID" "/piksi_buildroot/buildroot"
 
 [ -d "/piksi_buildroot/buildroot/output" ] && \
-  sudo chown "$USER:$USER" "/piksi_buildroot/buildroot/output"
+  sudo chown "$USER:$GID" "/piksi_buildroot/buildroot/output"
 
 [ -d "/piksi_buildroot/buildroot/output/images" ] && \
-  sudo chown "$USER:$USER" "/piksi_buildroot/buildroot/output/images"
+  sudo chown "$USER:$GID" "/piksi_buildroot/buildroot/output/images"
 
 exec sudo --preserve-env --user="$USER" --shell -- "$@"
