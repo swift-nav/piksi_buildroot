4d701cca2383a509f043877759d207b089e05bba
Colin Beighley <colinbeighley@gmail.com>
2017-12-11 21:28:29 -0800

    Release dependencies

--- fetch_firmware.sh
+++ fetch_firmware.sh
@@ -15,8 +15,8 @@
 
 set -xe
 
-FW_VERSION=${1:-v1.3.6}
-NAP_VERSION=${2:-v1.3.6}
+FW_VERSION=${1:-v1.3.7}
+NAP_VERSION=${2:-v1.3.7}
 
 FW_S3_PATH=s3://swiftnav-releases/piksi_firmware_private/$FW_VERSION/v3
 NAP_S3_PATH=s3://swiftnav-releases/piksi_fpga/$NAP_VERSION
