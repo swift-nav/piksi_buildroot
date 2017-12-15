5d552a14a312582e034a334a615a9dd5c5fba474
Jason Mobarak <jason@swiftnav.com>
2017-12-15 11:52:00 -0800

    Wrap entire package so it doesn't show in build output

--- package/sample_daemon/sample_daemon.mk
+++ package/sample_daemon/sample_daemon.mk
@@ -4,13 +4,13 @@
 #
 ################################################################################
 
+ifeq    ($(BR2_BUILD_SAMPLE_DAEMON),y)
+
 SAMPLE_DAEMON_VERSION = 0.1
 SAMPLE_DAEMON_SITE = \
   "${BR2_EXTERNAL_piksi_buildroot_PATH}/package/sample_daemon"
 SAMPLE_DAEMON_SITE_METHOD = local
 SAMPLE_DAEMON_DEPENDENCIES = czmq libsbp libpiksi
-
-ifeq    ($(BR2_BUILD_SAMPLE_DAEMON),y)
 
 define SAMPLE_DAEMON_INVOKE_MAKE
     $(MAKE) CROSS=$(TARGET_CROSS) LD=$(TARGET_LD) -C $(@D)/src all
@@ -19,10 +19,6 @@ endef
 define SAMPLE_DAEMON_INVOKE_INSTALL
     $(INSTALL) -D -m 0755 $(@D)/src/sample_daemon $(TARGET_DIR)/usr/bin
 endef
-
-BR2_ROOTFS_OVERLAY += "${SAMPLE_DAEMON_SITE}/overlay"
-
-endif # ($(BR2_BUILD_SAMPLE_DAEMON),y)
 
 define SAMPLE_DAEMON_BUILD_CMDS
 	$(SAMPLE_DAEMON_INVOKE_MAKE)
@@ -33,3 +29,7 @@ define SAMPLE_DAEMON_INSTALL_TARGET_CMDS
 endef
 
 $(eval $(generic-package))
+
+BR2_ROOTFS_OVERLAY += "${SAMPLE_DAEMON_SITE}/overlay"
+
+endif # ($(BR2_BUILD_SAMPLE_DAEMON),y)
