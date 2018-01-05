bf1ef1ca805400bdbc098a6caa5cc1931cc3ac8f
Mark Fine <mark.fine@gmail.com>
2018-01-05 12:44:42 -0800

    Release dependencies

--- fetch_firmware.sh
+++ fetch_firmware.sh
@@ -15,8 +15,8 @@
 
 set -xe
 
-FW_VERSION=${1:-v1.2.0-develop-2017122916}
-NAP_VERSION=${2:-v1.2.0-develop-2017122916}
+FW_VERSION=${1:-v1.2.0-develop-2018010520}
+NAP_VERSION=${2:-v1.2.0-develop-2018010520}
 
 FW_S3_PATH=s3://swiftnav-artifacts/piksi_firmware_private/$FW_VERSION/v3
 NAP_S3_PATH=s3://swiftnav-artifacts/piksi_fpga/$NAP_VERSION
