f599a151b5d3eac83bac4b78f0039ee9e7e7a40d
Steve Landers <slanders@carnegierobotics.com>
2018-01-10 19:41:59 -0500

    Added INS files, moved to an INS version that can handle calibration from persistent storage

--- package/libsbp/libsbp.mk
+++ package/libsbp/libsbp.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-LIBSBP_VERSION = v2.3.1
+LIBSBP_VERSION = v2.3.4
 LIBSBP_SITE = https://github.com/swift-nav/libsbp
 LIBSBP_SITE_METHOD = git
 LIBSBP_INSTALL_STAGING = YES
