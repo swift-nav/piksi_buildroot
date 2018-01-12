f5b4c7fa86ce7359fe210de51637a90cfdf1c381
Gareth McMullin <gareth@blacksphere.co.nz>
2018-01-12 14:23:42 +1300

    Log with SBP warning if Internet is not available.

--- board/piksiv3/rootfs-overlay/etc/init.d/network_poll.sh
+++ board/piksiv3/rootfs-overlay/etc/init.d/network_poll.sh
@@ -1,6 +1,13 @@
 #!/bin/ash
 
 while true; do
+  sleep 10
+  if [ x`cat /var/run/network_available` != "x1" ]; then
+    echo "No route to Internet" | sbp_log --warn
+  fi
+done &
+
+while true; do
   if ping -w 5 -c 1 8.8.8.8 >/dev/null 2>&1 || \
      ping -w 5 -c 1 114.114.114.114 > /dev/null 2>&1; then
     echo 1 > /var/run/network_available
