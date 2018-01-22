7db8da8a7748c5edbf2ef807c0ddba045d12d3ba
Benjamin Altieri <benaltieri@gmail.com>
2018-01-22 14:56:42 -0800

    Typo - mispelling in glo_bias_timer_callback

--- package/health_daemon/src/glo_bias_monitor.c
+++ package/health_daemon/src/glo_bias_monitor.c
@@ -66,7 +66,7 @@ static int glo_bias_timer_callback(health_monitor_t *monitor, void *context)
   log_fn_t log_fn = health_monitor_get_log(monitor);
   if (glo_bias_ctx.glonass_enabled) {
     log_fn(LOG_WARNING,
-           "Glonass Biases Msg Timeout - no biases msg receieved within %d sec window",
+           "Glonass Biases Msg Timeout - no biases msg received within %d sec window",
            GNSS_BIAS_ALERT_RATE_LIMIT/1000);
   }
   if (!glo_bias_ctx.glo_setting_read_resp)
