ce33e5ca1cd3567796b4a2feb8ca77e3134bd821
Luka Strizic <luka.strizic@swiftnav.com>
2018-01-23 16:11:16 -0800

    Add libnmea2000 that does NMEA2000 message handling
    Signed-off-by: Luka Strizic <luka.strizic@swiftnav.com>

---
+++ package/libnmea2000/libnmea2000.mk
@@ -0,0 +1,22 @@
+################################################################################
+#
+# libnmea2000
+#
+################################################################################
+
+LIBNMEA2000_VERSION = ecd56bcf57f90b860759626b9a248d9e05e7ba50
+LIBNMEA2000_SITE = https://github.com/ttlappalainen/NMEA2000.git
+LIBNMEA2000_SITE_METHOD = git
+LIBNMEA2000_INSTALL_STAGING = YES
+LIBFOO_CONF_OPTS = -DNMEA2000_MSG_DEBUG -DNMEA2000_BUF_DEBUG
+
+define LIBNMEA2000_INSTALL_TARGET_CMDS
+    for i in $(@D)/src/*.so; do $(INSTALL) $$i $(TARGET_DIR)/usr/lib/; done
+endef
+
+define LIBNMEA2000_INSTALL_STAGING_CMDS
+    for i in $(@D)/src/*.so; do $(INSTALL) $$i $(STAGING_DIR)/usr/lib/; done
+    for i in $(@D)/src/*.h; do $(INSTALL) $$i $(STAGING_DIR)/usr/include/; done
+endef
+
+$(eval $(cmake-package))
