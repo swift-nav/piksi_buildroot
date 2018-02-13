e605b1e667c5e7966c42a5bfe27f2edc1b9be5d8
Benjamin Altieri <benaltieri@gmail.com>
2018-02-13 13:00:36 -0800

    Added short term setting watch for simulation in rtcm3 bridge.

--- package/sbp_rtcm3_bridge/src/sbp.h
+++ package/sbp_rtcm3_bridge/src/sbp.h
@@ -15,6 +15,7 @@
 
 #include <libpiksi/sbp_zmq_rx.h>
 #include <libpiksi/sbp_zmq_tx.h>
+#include <libsbp/settings.h>
 
 #define TIME_SOURCE_MASK 0x07 /* Bits 0-2 */
 #define NO_TIME          0
@@ -22,6 +23,7 @@
 int sbp_init(sbp_zmq_rx_ctx_t *rx_ctx, sbp_zmq_tx_ctx_t *tx_ctx);
 void sbp_message_send(u16 msg_type, u8 len, u8 *payload, u16 sender_id);
 int sbp_callback_register(u16 msg_type, sbp_msg_callback_t cb, void *context);
+void sbp_read_resp_callback(u16 sender_id, u8 len, u8 msg_[], void *context);
 void sbp_base_obs_invalid(double timediff);
 
 #endif /* SWIFTNAV_SBP_H */
