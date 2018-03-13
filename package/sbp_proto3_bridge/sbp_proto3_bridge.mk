f37e381608d3c8bdce3f184251c69eb9a5f63dad
Keerthan Jaic <keerthan@swiftnav.com>
2018-03-12 17:41:44 -0700

    add sbp_proto3_bridge skeleton

---
+++ package/sbp_proto3_bridge/sbp_proto3_bridge.mk
@@ -0,0 +1,29 @@
+################################################################################
+#
+# sbp_proto3_bridge
+#
+################################################################################
+
+SBP_PROTO3_BRIDGE_VERSION = 0.1
+SBP_PROTO3_BRIDGE_SITE = \
+	"${BR2_EXTERNAL_piksi_buildroot_PATH}/package/sbp_proto3_bridge/src"
+SBP_PROTO3_BRIDGE_SITE_METHOD = local
+SBP_PROTO3_BRIDGE_DEPENDENCIES = czmq libsbp libpiksi
+
+define SBP_PROTO3_BRIDGE_BUILD_CMDS_DEFAULT
+	$(MAKE) CC=$(TARGET_CC) LD=$(TARGET_LD) -C $(@D) all
+endef
+
+define SBP_PROTO3_BRIDGE_BUILD_CMDS
+	$(SBP_PROTO3_BRIDGE_BUILD_CMDS_DEFAULT)
+endef
+
+define SBP_PROTO3_BRIDGE_INSTALL_TARGET_CMDS_DEFAULT
+	$(INSTALL) -D -m 0755 $(@D)/sbp_proto3_bridge $(TARGET_DIR)/usr/bin
+endef
+
+define SBP_PROTO3_BRIDGE_INSTALL_TARGET_CMDS
+	$(SBP_PROTO3_BRIDGE_INSTALL_TARGET_CMDS_DEFAULT)
+endef
+
+$(eval $(generic-package))
---
+++ package/sbp_proto3_bridge/src/main.c
@@ -0,0 +1,64 @@
+#include <assert.h>
+#include <czmq.h>
+#include <getopt.h>
+#include <libpiksi/sbp_zmq_pubsub.h>
+#include <libpiksi/sbp_zmq_rx.h>
+#include <libpiksi/settings.h>
+#include <libpiksi/util.h>
+#include <libpiksi/logging.h>
+#include <libsbp/navigation.h>
+#include <libsbp/sbp.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "sbp.h"
+
+
+#define PROGRAM_NAME "sbp_proto3_bridge"
+
+#define RTCM3_SUB_ENDPOINT  ">tcp://127.0.0.1:45010"  /* RTCM3 Internal Out */
+#define SBP_SUB_ENDPOINT    ">tcp://127.0.0.1:43030"  /* SBP External Out */
+#define SBP_PUB_ENDPOINT    ">tcp://127.0.0.1:43031"  /* SBP External In */
+
+
+// static int cleanup(settings_ctx_t **settings_ctx_loc,
+//                    sbp_zmq_pubsub_ctx_t **pubsub_ctx_loc,
+//                    int status) {
+//   sbp_zmq_pubsub_destroy(pubsub_ctx_loc);
+//   settings_destroy(settings_ctx_loc);
+//   logging_deinit();
+
+//   return status;
+// }
+
+
+static void utc_time_callback(u16 sender_id, u8 len, u8 msg[], void *context)
+{
+  (void) context;
+  (void) sender_id;
+  (void) len;
+  msg_utc_time_t *time = (msg_utc_time_t*)msg;
+
+  piksi_log(LOG_INFO, "utc time callback");
+}
+
+int main(int argc, char *argv[])
+{
+  logging_init(PROGRAM_NAME);
+
+
+  /* Prevent czmq from catching signals */
+  zsys_handler_set(NULL);
+
+  ctx = sbp_zmq_pubsub_create(SBP_PUB_ENDPOINT, SBP_SUB_ENDPOINT);
+  if (ctx == NULL) {
+    piksi_log(LOG_ERR, "settings_ctx error")
+    // return cleanup(&settings_ctx, &ctx, EXIT_FAILURE);
+  }
+
+  if (sbp_callback_register(SBP_MSG_UTC_TIME, utc_time_callback, NULL) != 0) {
+    piksi_log(LOG_ERR, "error setting UTC TIME callback");
+    // return cleanup(&settings_ctx, &ctx, EXIT_FAILURE);
+  }
+}
