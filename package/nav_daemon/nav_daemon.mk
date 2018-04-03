73ac2e9cb546777039d3876ab456fd49a46d5ce5
denniszollo <dzollo@swift-nav.com>
2018-04-03 23:36:27 +0000

    Add config launch and include nav_daemon strawman app

---
+++ package/nav_daemon/nav_daemon.mk
@@ -0,0 +1,21 @@
+################################################################################
+#
+# nav_daemon
+#
+################################################################################
+
+NAV_DAEMON_VERSION = 0.1
+NAV_DAEMON_SITE = \
+  "${BR2_EXTERNAL_piksi_buildroot_PATH}/package/nav_daemon"
+NAV_DAEMON_SITE_METHOD = local
+NAV_DAEMON_DEPENDENCIES = czmq libsbp libpiksi
+
+define NAV_DAEMON_INSTALL_TARGET_CMDS
+	$(INSTALL) -D -m 0755 $(@D)/src/nav_daemon $(TARGET_DIR)/usr/bin
+endef
+
+define NAV_DAEMON_BUILD_CMDS
+	$(MAKE) CROSS=$(TARGET_CROSS) LD=$(TARGET_LD) -C $(@D)/src all
+endef
+
+$(eval $(generic-package))
