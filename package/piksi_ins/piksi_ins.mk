e9ce810117eb0c313eb2e6628b20d5e211de87ab
denniszollo <dzollo@swift-nav.com>
2018-04-03 21:58:21 +0000

    Update to smoothpose github

--- package/piksi_ins/piksi_ins.mk
+++ package/piksi_ins/piksi_ins.mk
@@ -3,8 +3,8 @@
 # piksi_ins
 #
 #############################################################
-PIKSI_INS_VERSION = a04a36fd25ade13f8adb980c80c5b28074e534ee
-PIKSI_INS_SITE = ssh://git@endpoint.carnegierobotics.com/opt/git/piksi_ins.git
+PIKSI_INS_VERSION = master
+PIKSI_INS_SITE = git@github.com:carnegieroboticsllc/piksi_ins.git
 PIKSI_INS_SITE_METHOD = git
 PIKSI_INS_INSTALL_STAGING = YES
 PIKSI_INS_INSTALL_TARGET = YES
