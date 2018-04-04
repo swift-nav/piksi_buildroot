88298b6ed28dbb792498c5f856dd88b695ef8810
denniszollo <dzollo@swift-nav.com>
2018-04-04 23:12:58 +0000

    Use v1.5 smoothpose

--- package/piksi_ins/piksi_ins.mk
+++ package/piksi_ins/piksi_ins.mk
@@ -3,7 +3,7 @@
 # piksi_ins
 #
 #############################################################
-PIKSI_INS_VERSION = master
+PIKSI_INS_VERSION = v1.5 
 PIKSI_INS_SITE = git@github.com:carnegieroboticsllc/piksi_ins.git
 PIKSI_INS_SITE_METHOD = git
 PIKSI_INS_INSTALL_STAGING = YES
