7798c67ef5a85179a9f79a4c112f2dbd901cc4cb
Jason Mobarak <jason@swiftnav.com>
2018-04-07 15:49:18 -0700

    Fix bug with filter_out_config permissions

--- package/skylark_daemon/src/skylark_settings.c
+++ package/skylark_daemon/src/skylark_settings.c
@@ -49,7 +49,7 @@ static int skylark_upload_adapter_execfn(void) {
     "--file", UPLOAD_FIFO_FILE_PATH,
     "-s", ">tcp://127.0.0.1:43080",
     "--filter-out", "sbp",
-    "--filter-out-config", "/etc/skylark_upload_filter_out_config",
+    "--filter-out-config", "/etc/filter_out_config/skylark_upload",
     NULL,
   };
 
