a877ead59ae8023a910229009cb8bae728dc2450
Jason Mobarak <jason@swiftnav.com>
2018-04-19 22:04:07 -0700

    Fix recursive variable ref

--- external.mk
+++ external.mk
@@ -4,9 +4,9 @@ ifneq ($(BR2_DISABLE_LTO),)
 
 $(info *** Disabling compiler LTO pass... ***)
 
-BR2_TARGET_OPTIMIZATION = $(filter-out,$(BR2_TARGET_OPTIMIZATION),-flto)
-BR2_TARGET_CFLAGS = $(filter-out,$(BR2_TARGET_CFLAGS),-flto)
-BR2_TARGET_LDFLAGS = $(filter-out,$(BR2_TARGET_LDFLAGS),-flto)
+BR2_TARGET_OPTIMIZATION := $(filter-out,$(BR2_TARGET_OPTIMIZATION),-flto)
+BR2_TARGET_CFLAGS := $(filter-out,$(BR2_TARGET_CFLAGS),-flto)
+BR2_TARGET_LDFLAGS := $(filter-out,$(BR2_TARGET_LDFLAGS),-flto)
 
 else
 
