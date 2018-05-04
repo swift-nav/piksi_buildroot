fa1a69132ef25faf2a9347547a77e04c49adbf1e
Jason Mobarak <jason@swiftnav.com>
2018-05-04 16:18:14 -0700

    Experiment with different fs options for f2fs

--- board/piksiv3/rootfs-overlay/lib/mdev/automount.sh
+++ board/piksiv3/rootfs-overlay/lib/mdev/automount.sh
@@ -3,6 +3,7 @@
 destdir=/media
 log_tag=automount
 
+source /etc/init.d/sdcard.sh
 source /etc/init.d/logging.sh
 
 do_umount()
@@ -27,11 +28,17 @@ do_mount()
   local mountpoint="${destdir}/${devname}"
   local dev="/dev/${devname}"
 
+  local mount_options=
+
+  if detect_f2fs $devname; then
+    mount_options="-o data_flush,noextent_cache,sync"
+  fi
+
   logi "Mounting '${dev}' to '${mountpoint}'"
 
   mkdir -p $mountpoint || exit 1
 
-  if ! mount -t auto $dev $mountpoint 1>$logger_stdout 2>$logger_stderr; then
+  if ! mount -t auto $mount_options $dev $mountpoint 1>$logger_stdout 2>$logger_stderr; then
     loge "Mount failed, cleaning up mount point..."
     rmdir $mountpoint
     exit 1
