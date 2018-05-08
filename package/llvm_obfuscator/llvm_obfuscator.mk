70b7dbaeb3cc39bc7bcb1c09f3a114d087a9b60b
Jason Mobarak <jason@swiftnav.com>
2018-05-07 23:09:47 -0700

    Update llvm_obfuscator.mk
    + Remove sysroot sync
    
    The llvm toolchain only uses the buildroot toolchain now, so it's not
    necessary to stage the includes and libs that buildroot puts into the
    toolchain sysroot into the llvm sysroot.

--- package/llvm_obfuscator/llvm_obfuscator.mk
+++ package/llvm_obfuscator/llvm_obfuscator.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-HOST_LLVM_OBFUSCATOR_VERSION = v14
+HOST_LLVM_OBFUSCATOR_VERSION = v16
 HOST_LLVM_OBFUSCATOR_SOURCE = $(HOST_LLVM_OBFUSCATOR_VERSION)/llvm-obfuscator-arm-x86.txz
 HOST_LLVM_OBFUSCATOR_SITE = https://github.com/swift-nav/llvm-obfuscator-arm/releases/download/
 HOST_LLVM_OBFUSCATOR_ACTUAL_SOURCE_TARBALL = llvm-obfuscator-$(HOST_LLVM_OBFUSCATOR_VERSION).tar.xz
@@ -23,16 +23,8 @@ HOST_LLVM_OBFUSCATOR_PRE_EXTRACT_HOOKS += HOST_LLVM_OBFUSCATOR_PRE_EXTRACT_FIXUP
 SYSROOT = $(shell find $(HOST_DIR) -name 'sysroot' | grep -v llvm-obf)
 
 define HOST_LLVM_OBFUSCATOR_INSTALL_CMDS
-
 	mkdir -p $(HOST_DIR)/opt/llvm-obfuscator
 	rsync -az $(@D)/opt/llvm-obfuscator/ $(HOST_DIR)/opt/llvm-obfuscator/
-	rsync -az --ignore-existing \
-		$(SYSROOT)/lib/ $(HOST_DIR)/opt/llvm-obfuscator/sysroot/lib/
-	rsync -az --ignore-existing \
-		$(SYSROOT)/usr/lib/ $(HOST_DIR)/opt/llvm-obfuscator/sysroot/usr/lib/
-	rsync -az --ignore-existing \
-		$(SYSROOT)/usr/include/ $(HOST_DIR)/opt/llvm-obfuscator/sysroot/usr/include/
-
 endef
 
 LLVM_OBF_CC      = $(HOST_DIR)/opt/llvm-obfuscator/wrappers/bin/arm-linux-gnueabihf-clang
