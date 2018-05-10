c2ab4e649427f4f951ec77ec0c56f6c4474091d3
Jared Wilson <jared@swiftnav.com>
2018-05-10 13:48:55 -0700

    update pfwp pointer for variance tests

--- fetch_firmware.sh
+++ fetch_firmware.sh
@@ -27,10 +27,10 @@ if [[ $(uname -a) == *NixOS* ]]; then
   export LD_LIBRARY_PATH=/lib:/usr/lib
 fi
 
-FW_VERSION=${1:-v1.4.0-develop-2018050419}
-NAP_VERSION=${2:-v1.4.0-develop-2018050419}
+FW_VERSION=${1:-v1.4.0-develop-2018050419-10-g0db4cff1}
+NAP_VERSION=${2:-v1.4.10}
 
-FW_S3_PATH=s3://swiftnav-artifacts/piksi_firmware_private/$FW_VERSION/v3
+FW_S3_PATH=s3://swiftnav-artifacts-pull-requests/piksi_firmware_private/$FW_VERSION/v3
 NAP_S3_PATH=s3://swiftnav-artifacts/piksi_fpga/$NAP_VERSION
 export AWS_DEFAULT_REGION="us-west-2"
 
