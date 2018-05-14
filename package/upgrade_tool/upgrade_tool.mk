ab5c30d6c4f24352d898c2606292bf566ce3f268
Jason Mobarak <jason@swiftnav.com>
2018-05-14 12:17:01 -0700

    Use closed source version of upgrade_tool

--- package/upgrade_tool/upgrade_tool.mk
+++ package/upgrade_tool/upgrade_tool.mk
@@ -4,28 +4,13 @@
 #
 ################################################################################
 
-UPGRADE_TOOL_VERSION = 0.1
-UPGRADE_TOOL_SITE = \
-  "${BR2_EXTERNAL_piksi_buildroot_PATH}/package/upgrade_tool/src"
-UPGRADE_TOOL_SITE_METHOD = local
-UPGRADE_TOOL_DEPENDENCIES = uboot_custom zlib
-
-UPGRADE_TOOL_UBOOT_DIR = \
-	$(shell find $(BUILD_DIR) -maxdepth 1 -type d -name uboot_custom-*)
-
-define UPGRADE_TOOL_BUILD_CMDS
-	# copy source files from uboot folder
-	mkdir -p $(@D)/uboot
-	cp $(UPGRADE_TOOL_UBOOT_DIR)/include/image_table.h $(@D)/uboot
-	cp $(UPGRADE_TOOL_UBOOT_DIR)/include/factory_data.h $(@D)/uboot
-	cp $(UPGRADE_TOOL_UBOOT_DIR)/common/image_table.c $(@D)/uboot
-	cp $(UPGRADE_TOOL_UBOOT_DIR)/common/factory_data.c $(@D)/uboot
-
-	$(MAKE) CC=$(TARGET_CC) LD=$(TARGET_LD) -C $(@D) all
-endef
+UPGRADE_TOOL_VERSION = v1.4.0-develop-2018051217-31-g3487bf0
+UPGRADE_TOOL_SITE = git@github.com:swift-nav/piksi_upgrade_tool_bin.git
+UPGRADE_TOOL_SITE_METHOD = git
 
 define UPGRADE_TOOL_INSTALL_TARGET_CMDS
-	$(INSTALL) -D -m 0755 $(@D)/upgrade_tool $(TARGET_DIR)/usr/bin
+	$(MAKE) TARGET_DIR=$(TARGET_DIR) INSTALL=$(INSTALL) \
+		-C $(@D) install
 endef
 
 $(eval $(generic-package))
