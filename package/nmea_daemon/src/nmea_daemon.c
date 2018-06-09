f765e5ef23036728e82f3b2cb3a0ff2cfde74894
Jason Mobarak <jason@swiftnav.com>
2018-06-08 17:39:07 -0700

    Fix potential leak on error path

--- package/nmea_daemon/src/nmea_daemon.c
+++ package/nmea_daemon/src/nmea_daemon.c
@@ -112,7 +112,6 @@ static int nmea_reader_handler(zloop_t *zloop, zsock_t *zsock, void *arg)
 
   if (rename(tmp_file_name, NMEA_GGA_OUTPUT_PATH) < 0) {
     sbp_log(LOG_WARNING, "rename failed: %s", strerror(errno));
-    return 0;
   }
 
 exit_success:
