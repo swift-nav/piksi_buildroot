5779e69decbf17844ae142083331e2b3a237a699
Jason Mobarak <jason@swiftnav.com>
2018-06-11 15:03:01 -0700

    Remove czmq as a dep to piksi_ins

--- package/piksi_ins/piksi_ins.mk
+++ package/piksi_ins/piksi_ins.mk
@@ -14,7 +14,7 @@ PIKSI_INS_SITE = git@github.com:carnegieroboticsllc/piksi_ins.git
 PIKSI_INS_SITE_METHOD = git
 PIKSI_INS_INSTALL_STAGING = YES
 PIKSI_INS_INSTALL_TARGET = YES
-PIKSI_INS_DEPENDENCIES = czmq libsbp libpiksi eigen
+PIKSI_INS_DEPENDENCIES = libsbp libpiksi eigen
 
 $(eval $(cmake-package))
 
