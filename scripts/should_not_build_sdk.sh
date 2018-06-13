b5b34e7419a89b7b0d8d95510601c727044d7370
Jason Mobarak <jason@swiftnav.com>
2018-06-13 10:24:00 -0700

    debug

--- scripts/should_not_build_sdk.sh
+++ scripts/should_not_build_sdk.sh
@@ -1,20 +1,24 @@
 #!/bin/sh
 
+[[ -z "$DEBUG" ]] || set -x
+
 has_tag() {
   git describe --exact-match --tags &>/dev/null
 }
 
+should_build_sdk=
+
 if [ -n "$FORCE_SDK_BUILD" ]; then
   echo ">>> We *SHOULD* create a buildroot SDK because FORCE_SDK_BUILD was set..."
-  exit 1
+  should_build_sdk=y
 else
   echo ">>> Found that FORCE_SDK_BUILD was NOT set..."
 fi
 
-if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
+if [ "$TRAVIS_PULL_REQUEST" = "false" ]; then
   if has_tag; then
     echo ">>> We *SHOULD* create a buildroot SDK because this isn't a pull request, and we found a tag..."
-    exit 1
+    should_build_sdk=y
   else
     echo ">>> We *SHOULD NOT* create a buildroot SDK because this rev does not have a tag."
   fi
@@ -22,4 +26,5 @@ else
   echo ">>> We *SHOULD NOT* create a buildroot SDK because this is a PR build."
 fi
 
-exit 0
+# Invert to answer the "not" question
+[[ -n "$should_build_sdk" ]] && exit 1 || exit 0
