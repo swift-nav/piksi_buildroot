36e779a3324bbae72a2993cef119783640509443
Nicole Kelley <nicole.a.kelley@gmail.com>
2018-06-13 15:09:02 -0700

    Release dependencies

--- fetch_firmware.sh
+++ fetch_firmware.sh
@@ -27,8 +27,8 @@ if [[ $(uname -a) == *NixOS* ]]; then
   export LD_LIBRARY_PATH=/lib:/usr/lib
 fi
 
-FW_VERSION=${1:-v1.4.0-develop-2018061217}
-NAP_VERSION=${2:-v1.4.0-develop-2018061217}
+FW_VERSION=${1:-v1.4.0-develop-2018061321}
+NAP_VERSION=${2:-v1.4.0-develop-2018061321}
 
 FW_S3_PATH=s3://swiftnav-artifacts/piksi_firmware_private/$FW_VERSION/v3
 NAP_S3_PATH=s3://swiftnav-artifacts/piksi_fpga/$NAP_VERSION
