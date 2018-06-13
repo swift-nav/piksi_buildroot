ea581d50fa8ee61082588dc685f4147468363455
Jason Mobarak <jason@swiftnav.com>
2018-06-13 11:46:17 -0700

    Update should_not_build_sdk.sh

--- scripts/should_not_build_sdk.sh
+++ scripts/should_not_build_sdk.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
 [[ -z "$DEBUG" ]] || set -x
 
@@ -8,14 +8,14 @@ has_tag() {
 
 should_build_sdk=
 
-if [ -n "$FORCE_SDK_BUILD" ]; then
+if [[ -n "$FORCE_SDK_BUILD" ]]; then
   echo ">>> We *SHOULD* create a buildroot SDK because FORCE_SDK_BUILD was set..."
   should_build_sdk=y
 else
   echo ">>> Found that FORCE_SDK_BUILD was NOT set..."
 fi
 
-if [ "$TRAVIS_PULL_REQUEST" = "false" ]; then
+if [[ "$TRAVIS_PULL_REQUEST" == "false" ]]; then
   if has_tag; then
     echo ">>> We *SHOULD* create a buildroot SDK because this isn't a pull request, and we found a tag..."
     should_build_sdk=y
