a637e8842375fea4088cd60b922bafe616e393a2
Josh Kretzmer <jkretzmer@swift-nav.com>
2018-06-19 16:29:52 -0700

    updated piksi_ins to 1.6 and allowed * to proceed vX.X.X in get_git_string.sh

--- package/piksi_ins/piksi_ins.mk
+++ package/piksi_ins/piksi_ins.mk
@@ -9,7 +9,7 @@ ifeq    ($(BR2_BUILD_RELEASE_PROTECTED),y)
 
 $(info *** Piksi INS is enabled, packaging with current image...)
 
-PIKSI_INS_VERSION = master
+PIKSI_INS_VERSION = v1.6
 PIKSI_INS_SITE = git@github.com:carnegieroboticsllc/piksi_ins.git
 PIKSI_INS_SITE_METHOD = git
 PIKSI_INS_INSTALL_STAGING = YES
