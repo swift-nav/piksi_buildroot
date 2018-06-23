fed9852abe81ae0748759a00e77d816f32996890
Nicole Kelley <nicole.a.kelley@gmail.com>
2018-06-22 20:36:05 -0700

    Release dependencies

--- fetch_firmware.sh
+++ fetch_firmware.sh
@@ -27,13 +27,13 @@ if [[ $(uname -a) == *NixOS* ]]; then
   export LD_LIBRARY_PATH=/lib:/usr/lib
 fi
 
-FW_VERSION=${1:-v1.6.1}
-NAP_VERSION=${2:-v1.6.1}
+FW_VERSION=${1:-v1.6.2}
+NAP_VERSION=${2:-v1.6.2}
 
 FW_S3_PATH=s3://swiftnav-releases/piksi_firmware_private/$FW_VERSION/v3
 
 NAP_S3_PATH=s3://swiftnav-artifacts/piksi_fpga/$NAP_VERSION
-NAP_S3_SDK_PATH=s3://swiftnav-releases/piksi_fpga/$NAP_VERSION
+NAP_SDK_S3_PATH=s3://swiftnav-releases/piksi_fpga/$NAP_VERSION
 
 export AWS_DEFAULT_REGION="us-west-2"
 
