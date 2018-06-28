67c7c64e5de200891457b975cc5d72dcaeedc2b2
Jason Mobarak <jason@swiftnav.com>
2018-06-28 14:31:49 -0700

    Add cmph, prep endpoint_router for tests

--- package/endpoint_router/endpoint_router.mk
+++ package/endpoint_router/endpoint_router.mk
@@ -5,16 +5,16 @@
 ################################################################################
 
 ENDPOINT_ROUTER_VERSION = 0.1
-ENDPOINT_ROUTER_SITE = "${BR2_EXTERNAL_piksi_buildroot_PATH}/package/endpoint_router/src"
+ENDPOINT_ROUTER_SITE = "${BR2_EXTERNAL_piksi_buildroot_PATH}/package/endpoint_router/endpoint_router"
 ENDPOINT_ROUTER_SITE_METHOD = local
 ENDPOINT_ROUTER_DEPENDENCIES = libuv nanomsg libsbp libpiksi libyaml
 
 define ENDPOINT_ROUTER_BUILD_CMDS
-    $(MAKE) CC=$(TARGET_CC) LD=$(TARGET_LD) -C $(@D) all
+    $(MAKE) CC=$(TARGET_CC) LD=$(TARGET_LD) CFLAGS="$(TARGET_CFLAGS)" -C $(@D) all
 endef
 
 define ENDPOINT_ROUTER_INSTALL_TARGET_CMDS
-    $(INSTALL) -D -m 0755 $(@D)/endpoint_router $(TARGET_DIR)/usr/bin
+    $(INSTALL) -D -m 0755 $(@D)/src/endpoint_router $(TARGET_DIR)/usr/bin
     $(INSTALL) -d -m 0755 $(TARGET_DIR)/etc/endpoint_router
 endef
 
