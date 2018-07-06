5e63044b79e6304723c71e182c6f3d8e87c29dd0
Jason Mobarak <jason@swiftnav.com>
2018-07-06 02:23:12 -0700

    Try using ipc instead of tcp

--- package/ntrip_daemon/src/ntrip_settings.c
+++ package/ntrip_daemon/src/ntrip_settings.c
@@ -87,7 +87,7 @@ static int ntrip_adapter_execfn(void) {
     "endpoint_adapter",
     "-f", "rtcm3",
     "--file", FIFO_FILE_PATH,
-    "-p", "tcp://127.0.0.1:45031",
+    "-p", "ipc:///tmp/tmp.45031",
     NULL,
   };
 
