c2f27d1cfeef795ba89ccec382aaf146f232322b
Jason Mobarak <jason@swiftnav.com>
2018-07-12 19:52:01 -0700

    Use a newer version of nanomsg

--- package/endpoint_adapter/endpoint_adapter.mk
+++ package/endpoint_adapter/endpoint_adapter.mk
@@ -8,7 +8,7 @@ ENDPOINT_ADAPTER_VERSION = 0.1
 ENDPOINT_ADAPTER_SITE = \
   "${BR2_EXTERNAL_piksi_buildroot_PATH}/package/endpoint_adapter/src"
 ENDPOINT_ADAPTER_SITE_METHOD = local
-ENDPOINT_ADAPTER_DEPENDENCIES = libuv nanomsg libsbp libpiksi
+ENDPOINT_ADAPTER_DEPENDENCIES = libuv nanomsg_custom libsbp libpiksi
 
 define ENDPOINT_ADAPTER_BUILD_CMDS
     $(MAKE) CC=$(TARGET_CC) LD=$(TARGET_LD) -C $(@D) all
