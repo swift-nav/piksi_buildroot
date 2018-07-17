ecfb88f1b4d86c7dcb060d23a89e4762f318500e
mbavaro <michele@swiftnav.com>
2018-07-17 05:29:58 -0700

    Removed fpga_error_moni, added pfw_welcome (#714) (#718)
    * removed fpga_error_moni, added pfw_welcome (#714)
    
    * restored spaces

---
+++ package/pfw_welcome/pfw_welcome.mk
@@ -0,0 +1,21 @@
+################################################################################
+#
+# pfw_welcome
+#
+################################################################################
+
+PFW_WELCOME_VERSION = 0.1
+PFW_WELCOME_SITE = \
+  "${BR2_EXTERNAL_piksi_buildroot_PATH}/package/pfw_welcome/src"
+PFW_WELCOME_SITE_METHOD = local
+PFW_WELCOME_DEPENDENCIES = libuv nanomsg_custom libpiksi
+
+define PFW_WELCOME_BUILD_CMDS
+    $(MAKE) CC=$(TARGET_CC) LD=$(TARGET_LD) -C $(@D) all
+endef
+
+define PFW_WELCOME_INSTALL_TARGET_CMDS
+    $(INSTALL) -D -m 0755 $(@D)/pfw_welcome $(TARGET_DIR)/usr/bin
+endef
+
+$(eval $(generic-package))
--- package/pfw_welcome/src/pfw_welcome.c
+++ package/pfw_welcome/src/pfw_welcome.c
@@ -27,83 +27,35 @@
 #include <libsbp/system.h>
 #include <libsbp/logging.h>
 
-#define PROGRAM_NAME "fpga_error_moni"
+#define PROGRAM_NAME "pfw_welcome"
 
 #define SBP_SUB_ENDPOINT    "ipc:///var/run/sockets/external.pub"  /* SBP External Out */
 #define SBP_PUB_ENDPOINT    "ipc:///var/run/sockets/external.sub"  /* SBP External In */
 
-bool print_debug = false;
+static pk_loop_t *loop = NULL;
 
-static void usage(char *command) {
-  printf("Usage: %s\n", command);
-
-  puts("\nOptions:");
-  puts("\t--debug   Enable debug messages");
-}
-
-static int parse_options(int argc, char *argv[]) {
-  enum {
-    OPT_ID_DEBUG = 1,
-  };
-
-  const struct option long_opts[] = {
-    {"debug", no_argument, 0, OPT_ID_DEBUG},
-    {0, 0, 0, 0},
-  };
-
-  int opt;
-  while ((opt = getopt_long(argc, argv, "", long_opts, NULL)) != -1) {
-    switch (opt) {
-      case OPT_ID_DEBUG:
-        print_debug = true;
-        break;
-      default:
-        puts("Invalid option");
-        return -1;
-    }
-  }
-  return 0;
-}
-
-#define MSG_LOG_LEVEL_WARN 4
-#define DONE_PIN_FILE "/sys/devices/soc0/amba/f8007000.devcfg/prog_done"
-
-static void info_callback(u16 sender_id, u8 len, u8 msg[], void *context)
-{
+static void heartbeat_callback(u16 sender_id, u8 len, u8 msg[], void *context) {
   (void) sender_id;
   (void) len;
   (void) context;
-  char line[16];
-  FILE *fid;
 
-  msg_log_t *msg_log = (msg_log_t*)msg;
+  msg_heartbeat_t *msg_hb = (msg_heartbeat_t*) msg;
 
-  if (msg_log->level < MSG_LOG_LEVEL_WARN) {
-    if (NULL != strstr(msg_log->text, "NAP Verification Failed")) {
-      fid = fopen(DONE_PIN_FILE, "rt");
-      if (NULL == fid) {
-        sbp_log(LOG_ERR, "could not open %s", DONE_PIN_FILE);
-        return;
-      }
-      fgets(line, 16, fid);
-      fclose(fid); fid = NULL;
-      sbp_log(LOG_ERR, "cat %s -> %s", DONE_PIN_FILE, line);
-    }
+  if (msg_hb->flags & 0x7) {
+    sbp_log(LOG_ERR, "firmware claiming an error state: 0x%08x", msg_hb->flags);
   }
+
+  pk_loop_stop(loop);
 }
 
 int main(int argc, char *argv[]) {
+  (void) argc;
+  (void) argv;
+
   int status = EXIT_SUCCESS;
-  pk_loop_t *loop = NULL;
   sbp_pubsub_ctx_t *ctx = NULL;
 
   logging_init(PROGRAM_NAME);
-
-  if (parse_options(argc, argv) != 0) {
-    usage(argv[0]);
-    status = EXIT_FAILURE;
-    goto cleanup;
-  }
 
   sbp_log(LOG_INFO, PROGRAM_NAME " launched");
 
@@ -120,9 +72,8 @@ int main(int argc, char *argv[]) {
   }
 
   sbp_rx_ctx_t *rx_ctx = sbp_pubsub_rx_ctx_get(ctx);
-  sbp_tx_ctx_t *tx_ctx = sbp_pubsub_tx_ctx_get(ctx);
 
-  if ((NULL == rx_ctx) || (NULL == tx_ctx)) {
+  if ((NULL == rx_ctx)) {
     sbp_log(LOG_ERR, "Error initializing SBP!");
     status = EXIT_FAILURE;
     goto cleanup;
@@ -133,8 +84,8 @@ int main(int argc, char *argv[]) {
     goto cleanup;
   }
 
-  if (sbp_rx_callback_register(rx_ctx, SBP_MSG_LOG, info_callback, ctx, NULL) != 0) {
-    sbp_log(LOG_ERR, "Error setting SBP_MSG_LOG callback!");
+  if (sbp_rx_callback_register(rx_ctx, SBP_MSG_HEARTBEAT, heartbeat_callback, ctx, NULL) != 0) {
+    sbp_log(LOG_ERR, "Error setting SBP_MSG_HEARTBEAT callback!");
     status = EXIT_FAILURE;
     goto cleanup;
   }
