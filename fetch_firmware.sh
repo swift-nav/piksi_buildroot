2184dd6ab42d29489f83ed4677631401d8b65477
Nicole Kelley <nicole.a.kelley@gmail.com>
2018-07-27 15:46:02 -0700

    Release Tag v1.5.0-develop-2018072722

--- fetch_firmware.sh
+++ fetch_firmware.sh
@@ -27,8 +27,8 @@ if [[ $(uname -a) == *NixOS* ]]; then
   export LD_LIBRARY_PATH=/lib:/usr/lib
 fi
 
-FW_VERSION=${1:-v1.5.0-develop-2018072323}
-NAP_VERSION=${2:-v1.5.0-develop-2018072323}
+FW_VERSION=${1:-v1.5.0-develop-2018072722}
+NAP_VERSION=${2:-v1.5.0-develop-2018072722}
 
 FW_S3_PATH=s3://swiftnav-artifacts/piksi_firmware_private/$FW_VERSION/v3
 NAP_S3_PATH=s3://swiftnav-artifacts/piksi_fpga/$NAP_VERSION
