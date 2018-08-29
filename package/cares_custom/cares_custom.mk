37a4ed6a9a129aedf9ab1c6b14048f9720201fa7
Mark Fine <mark.fine@gmail.com>
2018-08-29 16:21:56 -0700

    Cares tweak

--- package/cares_custom/cares_custom.mk
+++ package/cares_custom/cares_custom.mk
@@ -5,8 +5,8 @@
 ################################################################################
 
 CARES_CUSTOM_VERSION = 1.13.0
-CARES_CUSTOM_SOURCE = c-ares-1.13.0.tar.gz
-CARES_CUSTOM_SITE = https://c-ares.haxx.se/download/c-ares-1.13.0.tar.gz
+CARES_CUSTOM_SOURCE = c-ares-$(CARES_CUSTOM_VERSION).tar.gz
+CARES_CUSTOM_SITE = https://c-ares.haxx.se/download
 CARES_CUSTOM_INSTALL_STAGING = YES
 CARES_CUSTOM_CONF_OPTS = --with-random=/dev/urandom
 # Rebuild configure to avoid XC_CHECK_USER_CFLAGS
