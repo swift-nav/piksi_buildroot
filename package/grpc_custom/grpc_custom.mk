514174068f6dfc800eb7f88ab326f52371140523
Mark Fine <mark.fine@gmail.com>
2018-08-29 19:55:09 -0700

    wip

--- package/grpc_custom/grpc_custom.mk
+++ package/grpc_custom/grpc_custom.mk
@@ -11,13 +11,12 @@ GRPC_CUSTOM_LICENSE = BSD-3-Clause
 GRPC_CUSTOM_LICENSE_FILES = LICENSE
 
 GRPC_CUSTOM_DEPENDENCIES = gflags gtest cares_custom openssl protobuf_custom zlib
+HOST_GRPC_DEPENDENCIES = host-cares_custom host-protobuf_custom host-openssl
 
 GRPC_CUSTOM_INSTALL_STAGING = YES
 
 GRPC_CUSTOM_MAKE_ENV = \
 	$(TARGET_MAKE_ENV) \
-	GRPC_CROSS_COMPILE="true" \
-	GRPC_CROSS_LDOPTS="-L$(HOST_DIR)/usr/lib" \
 	LDCONFIG=/bin/true \
 	CC="$(TARGET_CC)" \
 	CXX="$(TARGET_CXX)" \
@@ -55,4 +54,30 @@ define GRPC_CUSTOM_INSTALL_TARGET_CMDS
 		$(GRPC_CUSTOM_INSTALL_TARGETS)
 endef
 
+HOST_GRPC_CUSTOM_MAKE_OPTS = \
+	$(HOST_MAKE_ENV) \
+	CC="$(HOSTCC)" \
+	CXX="$(HOSTCXX)" \
+	LD="$(HOSTCC)" \
+	LDXX="$(HOSTCXX)" \
+	CPPFLAGS="$(HOST_CPPFLAGS)" \
+	CFLAGS="$(HOST_CFLAGS)" \
+	CXXFLAGS="$(HOST_CXXFLAGS)" \
+	LDFLAGS="$(HOST_LDFLAGS)" \
+	STRIP=/bin/true \
+	PROTOC="$(HOST_DIR)/usr/bin/protoc" \
+	prefix="$(HOST_DIR)"
+
+define HOST_GRPC_CUSTOM_BUILD_CMDS
+	$(HOST_MAKE_ENV) $(MAKE) $(HOST_GRPC_CUSTOM_MAKE_OPTS) -C $(@D) \
+		plugins
+endef
+
+define HOST_GRPC_CUSTOM_INSTALL_CMDS
+	$(HOST_MAKE_ENV) $(MAKE) $(HOST_GRPC_CUSTOM_MAKE_OPTS) -C $(@D) \
+		install-plugins
+endef
+
+
 $(eval $(generic-package))
+$(eval $(host-generic-package))
