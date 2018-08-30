7cf6ff6c474552a7e30ba191d5fae688e2c5b96c
Mark Fine <mark.fine@gmail.com>
2018-08-29 22:42:16 -0700

    Just grpc.

--- package/grpc/grpc.mk
+++ package/grpc/grpc.mk
@@ -4,13 +4,13 @@
 #
 ################################################################################
 
-GRPC_CMAKE_VERSION = v1.12.0
-GRPC_CMAKE_SITE = https://github.com/grpc/grpc.git
-GRPC_CMAKE_SITE_METHOD = git
-GRPC_CMAKE_LICENSE = BSD-3-Clause
-GRPC_CMAKE_LICENSE_FILES = LICENSE
-GRPC_CMAKE_DEPENDENCIES = gflags gtest c-ares openssl protobuf zlib
-GRPC_CMAKE_CONF_OPTS = \
+GRPC_VERSION = v1.12.0
+GRPC_SITE = https://github.com/grpc/grpc.git
+GRPC_SITE_METHOD = git
+GRPC_LICENSE = BSD-3-Clause
+GRPC_LICENSE_FILES = LICENSE
+GRPC_DEPENDENCIES = gflags gtest c-ares openssl protobuf zlib
+GRPC_CONF_OPTS = \
 	-DgRPC_ZLIB_PROVIDER=package \
 	-DgRPC_CARES_PROVIDER=package \
 	-DgRPC_SSL_PROVIDER=package
