67aa4a96196ade3856655cd251db412d77c488c3
anthony <anthony@swiftnav.com>
2018-08-30 20:45:10 -0700

    Add settings watch for NMEA rate settings

--- package/sbp_nmea_bridge/src/sbp_nmea_bridge.c
+++ package/sbp_nmea_bridge/src/sbp_nmea_bridge.c
@@ -13,6 +13,7 @@
 #include <assert.h>
 #include <getopt.h>
 #include <libpiksi/logging.h>
+#include <libpiksi/util.h>
 #include <libsbp/navigation.h>
 #include <libsbp/sbp.h>
 #include <stdint.h>
@@ -32,25 +33,31 @@ struct sbp_nmea_state state;
 
 pk_endpoint_t *nmea_pub = NULL;
 
-static int gpgga_rate;
+static int gpgga_rate = 1;
+static int gprmc_rate = 10;
+static int gpvtg_rate = 1;
+static int gphdt_rate = 1;
+static int gpgll_rate = 10;
+static int gpzda_rate = 10;
+static int gsa_rate = 10;
 
-static void usage(char *command)
-{
+static float soln_freq = 10.0;
+
+static void usage(char *command) {
   printf("Usage: %s\n", command);
 
   puts("\nMisc options");
   puts("\t--debug");
 }
 
-static void nmea_callback(uint8_t nmea_str[]){
+static void nmea_callback(uint8_t nmea_str[]) {
   size_t nmea_str_len = strlen((const char *)nmea_str);
   if (pk_endpoint_send(nmea_pub, nmea_str, nmea_str_len) != 0) {
     piksi_log(LOG_ERR, "Error sending nmea string to nmea_internal");
   }
 }
 
-static int parse_options(int argc, char *argv[])
-{
+static int parse_options(int argc, char *argv[]) {
   enum {
     OPT_ID_DEBUG = 1,
   };
@@ -78,15 +85,55 @@ static int parse_options(int argc, char *argv[])
   return 0;
 }
 
-static int notify_gpgga_rate_changed(void *context)
-{
+static int notify_gpgga_rate_changed(void *context) {
   (void)context;
   sbp2nmea_set_gpgga_rate(gpgga_rate, &state);
   return 0;
 }
 
-static void gps_time_callback(u16 sender_id, u8 len, u8 msg[], void *context)
-{
+static int notify_gprmc_rate_changed(void *context) {
+  (void)context;
+  sbp2nmea_set_gprmc_rate(gprmc_rate, &state);
+  return 0;
+}
+
+static int notify_gpvtg_rate_changed(void *context) {
+  (void)context;
+  sbp2nmea_set_gpvtg_rate(gpvtg_rate, &state);
+  return 0;
+}
+
+static int notify_gphdt_rate_changed(void *context) {
+  (void)context;
+  sbp2nmea_set_gphdt_rate(gphdt_rate, &state);
+  return 0;
+}
+
+static int notify_gpgll_rate_changed(void *context) {
+  (void)context;
+  sbp2nmea_set_gpgll_rate(gpgll_rate, &state);
+  return 0;
+}
+
+static int notify_gpzda_rate_changed(void *context) {
+  (void)context;
+  sbp2nmea_set_gpzda_rate(gpzda_rate, &state);
+  return 0;
+}
+
+static int notify_gsa_rate_changed(void *context) {
+  (void)context;
+  sbp2nmea_set_gsa_rate(gsa_rate, &state);
+  return 0;
+}
+
+static int notify_soln_freq_changed(void *context) {
+  (void)context;
+  sbp2nmea_set_soln_freq(soln_freq, &state);
+  return 0;
+}
+
+static void gps_time_callback(u16 sender_id, u8 len, u8 msg[], void *context) {
   (void) context;
   (void) sender_id;
   (void) len;
@@ -94,14 +141,19 @@ static void gps_time_callback(u16 sender_id, u8 len, u8 msg[], void *context)
   sbp2nmea_gps_time(gps_time, &state);
 }
 
-#if 0
-static void gps_time_reference_callback(u16 sender_id, u8 len, u8 msg[], void* context) {
+
+static void msg_obs_callback(u16 sender_id, u8 len, u8 msg[], void* context) {
   (void) len;
   (void) msg;
   (void) context;
-  sbp2nmea_set_base_id(sender_id, &state);
+  /* Must be outbound SBP sending */
+  if(sbp_sender_id_get() == sender_id) {
+    uint8_t num_obs = (len - sizeof(observation_header_t))/sizeof(packed_obs_content_t);
+    sbp2nmea_obs((msg_obs_t *)msg, num_obs, &state);
+  } else {
+    sbp2nmea_set_base_id(sender_id, &state);
+  }
 }
-#endif
 
 static void utc_time_callback(u16 sender_id, u8 len, u8 msg[], void *context)
 {
@@ -176,6 +228,11 @@ int main(int argc, char *argv[])
     exit(cleanup(EXIT_FAILURE));
   }
 
+  if (sbp_callback_register(SBP_MSG_OBS, msg_obs_callback, NULL) != 0) {
+    piksi_log(LOG_ERR, "error setting MSG OBS callback");
+    exit(cleanup(EXIT_FAILURE));
+  }
+
   if (sbp_callback_register(SBP_MSG_GPS_TIME, gps_time_callback, NULL) != 0) {
     piksi_log(LOG_ERR, "error setting GPS TIME callback");
     exit(cleanup(EXIT_FAILURE));
@@ -208,12 +265,45 @@ int main(int argc, char *argv[])
 
   settings_ctx = sbp_get_settings_ctx();
 
-  settings_add_watch(settings_ctx, "nmea", "gpgga_rate",
-                     &gpgga_rate , sizeof(gpgga_rate),
-                     SETTINGS_TYPE_INT,
-                     notify_gpgga_rate_changed, NULL);
+  settings_register(settings_ctx, "nmea", "gpgga_rate",
+                    &gpgga_rate , sizeof(gpgga_rate),
+                    SETTINGS_TYPE_INT,
+                    notify_gpgga_rate_changed, NULL);
 
+  settings_register(settings_ctx, "nmea", "gprmc_rate",
+                    &gprmc_rate , sizeof(gprmc_rate),
+                    SETTINGS_TYPE_INT,
+                    notify_gprmc_rate_changed, NULL);
 
+  settings_register(settings_ctx, "nmea", "gpvtg_rate",
+                    &gpvtg_rate , sizeof(gpvtg_rate),
+                    SETTINGS_TYPE_INT,
+                    notify_gpvtg_rate_changed, NULL);
+
+  settings_register(settings_ctx, "nmea", "gphdt_rate",
+                    &gphdt_rate , sizeof(gphdt_rate),
+                    SETTINGS_TYPE_INT,
+                    notify_gphdt_rate_changed, NULL);
+
+  settings_register(settings_ctx, "nmea", "gpgll_rate",
+                    &gpgll_rate, sizeof(gpgll_rate),
+                    SETTINGS_TYPE_INT,
+                    notify_gpgll_rate_changed, NULL);
+
+  settings_register(settings_ctx, "nmea", "gpzda_rate",
+                    &gpzda_rate, sizeof(gphdt_rate),
+                    SETTINGS_TYPE_INT,
+                    notify_gpzda_rate_changed, NULL);
+
+ settings_register(settings_ctx, "nmea", "gsa_rate",
+                   &gsa_rate, sizeof(gsa_rate),
+                   SETTINGS_TYPE_INT,
+                   notify_gsa_rate_changed, NULL);
+
+ settings_add_watch(settings_ctx, "solution", "soln_freq",
+                    &soln_freq, sizeof(soln_freq),
+                    SETTINGS_TYPE_FLOAT,
+                    notify_soln_freq_changed, NULL);
   sbp_run();
 
   exit(cleanup(EXIT_SUCCESS));
