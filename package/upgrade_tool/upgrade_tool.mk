86fb9af3906ebe3fd19ddc5e87f2b7b12dd99105
Jason Mobarak <jason@swiftnav.com>
2018-08-30 18:06:04 -0700

    Add upgrade_data dir for encrypted upgrades [DEVC-1055] (#791)
    * Add upgrade_data dir for encrypted upgrades
    
    * Update upgrade_tool to v1.5.0-develop-2018083010
    
    * Fix naming of new tmpfs drive

--- package/upgrade_tool/upgrade_tool.mk
+++ package/upgrade_tool/upgrade_tool.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-UPGRADE_TOOL_VERSION = v1.4.0-develop-2018061323
+UPGRADE_TOOL_VERSION = v1.5.0-develop-2018083010
 UPGRADE_TOOL_SITE = $(call github,swift-nav,piksi_upgrade_tool_bin,$(UPGRADE_TOOL_VERSION))
 
 define UPGRADE_TOOL_INSTALL_TARGET_CMDS
