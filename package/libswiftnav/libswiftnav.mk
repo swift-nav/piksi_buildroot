88e23f42dcad988a9c3832ab9f9b3c9bbf557ba0
Niilo Sirola <niilo.sirola@exafore.com>
2018-09-04 14:23:39 +0300

    add libswiftnav, librtcm and gnss_convertors to temp branches

---
+++ package/libswiftnav/libswiftnav.mk
@@ -0,0 +1,12 @@
+################################################################################
+#
+# libswiftnav
+#
+################################################################################
+
+LIBSWIFTNAV_VERSION = v0.1.1
+LIBSWIFTNAV_SITE = https://github.com/swift-nav/libswiftnav
+LIBSWIFTNAV_SITE_METHOD = git
+LIBSWIFTNAV_INSTALL_STAGING = YES
+
+$(eval $(cmake-package))
