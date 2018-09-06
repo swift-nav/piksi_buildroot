065a954b5f134984ec3cdd7cff304e7186455635
Pasi Miettinen <pmiettinen@exafore.com>
2018-09-06 09:29:56 +0300

    Set time limit for ntrip_init

--- package/libnetwork/src/libnetwork.c
+++ package/libnetwork/src/libnetwork.c
@@ -43,6 +43,11 @@
 /** Maximum length of url string */
 #define LIBNETWORK_URL_MAX_LENGTH (4096L)
 
+#define NTRIP_INIT_TIMEOUT_S (200L)
+#define NTRIP_INIT_RETRY_COOLDOWN_US (200000L)
+#define NTRIP_INIT_RETRY_COUNT_MAX \
+    (NTRIP_INIT_RETRY_COOLDOWN_US / NTRIP_INIT_TIMEOUT_S)
+
 /** How large to configure the recv buffer to avoid excessive buffering. */
 const long RECV_BUFFER_SIZE = 4096L;
 /** Max number of callbacks from CURLOPT_XFERINFOFUNCTION before we attempt to
@@ -928,8 +933,9 @@ static struct curl_slist *ntrip_init(network_context_t *ctx, CURL *curl)
   chunk = curl_slist_append(chunk, "Ntrip-Version: Ntrip/2.0");
   chunk = curl_slist_append(chunk, "Expect:");
 
-  while (!device_has_gps_time()) {
-    sleep(1);
+  int retries = 0;
+  while (!device_has_gps_time() && NTRIP_INIT_RETRY_COUNT_MAX > retries++) {
+    usleep(NTRIP_INIT_RETRY_COOLDOWN_US);
   }
   piksi_log(LOG_DEBUG|LOG_SBP, "ntrip_init: device has gps time");
 
