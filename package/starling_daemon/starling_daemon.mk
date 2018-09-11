29829c75a97fe17cc20e16f3366ebe88047c08e0
Kevin Dade <kevin@swiftnav.com>
2018-09-11 16:24:51 -0700

    Add skeleton make fragment for starling daemon.

---
+++ package/starling_daemon/starling_daemon.mk
@@ -0,0 +1,29 @@
+################################################################################
+#
+# starling_daemon
+#
+################################################################################
+
+ifeq    ($(BR2_BUILD_STARLING_DAEMON),y)
+
+STARLING_DAEMON_VERSION = 0.1.0
+STARLING_DAEMON_SITE = "${BR2_EXTERNAL_piksi_buildroot_PATH}/package/starling_daemon"
+STARLING_DAEMON_SITE_METHOD = local
+STARLING_DAEMON_DEPENDENCIES = libuv libsbp libpiksi
+
+# KEVIN NEEDS HELP
+define SAMPLE_DAEMON_INSTALL_TARGET_CMDS
+	$(INSTALL) *.so /usr/lib/
+	$(INSTALL) piksi-multi-linux-starling /usr/bin
+endef
+
+# KEVIN NEEDS HELP
+define SAMPLE_DAEMON_BUILD_CMDS
+	${STARLING_DAEMON_SITE}/fetch_and_extract_tarball_from_github_releases.sh
+endef
+
+BR2_ROOTFS_OVERLAY += "${STARLING_DAEMON_SITE}/overlay"
+
+$(eval $(generic-package))
+
+endif # ($(BR2_BUILD_STARLING_DAEMON),y)
