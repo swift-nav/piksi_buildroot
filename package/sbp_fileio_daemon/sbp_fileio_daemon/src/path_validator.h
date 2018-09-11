7b5b82ff0d7bf4ef817ad39911e4ea4939854f6b
Jason Mobarak <jason.mobarak@gmail.com>
2018-09-11 12:43:27 -0700

    Add docs

--- package/sbp_fileio_daemon/sbp_fileio_daemon/src/path_validator.h
+++ package/sbp_fileio_daemon/sbp_fileio_daemon/src/path_validator.h
@@ -26,16 +26,55 @@ typedef struct path_validator_s path_validator_t;
 
 /**
  * @brief Create a path validator, only fails if memory cannot be allocated.
+ *
+ * @details The path_validator object is used to validate a list of paths that
+ *   are allowed for some operation, in the case of the fileio daemon, these
+ *   paths that are allowed for read/write/list/etc.
  */
-path_validator_t *path_validator_create(void);
+path_validator_t *path_validator_create();
 
+/**
+ * @brief Destroys a path validator object and frees all memory associated.
+ *
+ * @param pctx pointer to a path_validator point, NULL'd on return.
+ */
 void path_validator_destroy(path_validator_t **pctx);
 
+/**
+ * @brief Check if a given path is in the list of allowed path prefixes.
+ *
+ * @details Traverses the list of paths specified with `path_validator_allow_path`
+ *   and checks if any path prefix matches, returns `true` in that case.
+ *
+ *   Input paths need not be "prefixed" with "/", since the daemon runs at the
+ *   root of the filesystem, the path_validator will automatically strip the
+ *   "/" to check if the path is allowed.
+ *
+ *   Input paths must already exist (or their parent directories must exist) for
+ *   the path to be considered allowed.
+ */
 bool path_validator_check(path_validator_t *ctx, const char* path);
 
+/**
+ * @brief Add a path prefix to the list of allowed path prefixes.
+ *
+ * @details The `path` parameter is copied to an internal buffer, and added to
+ *   a list owned by the `path_validator` object.
+ */
 void path_validator_allow_path(path_validator_t *ctx, const char* path);
+
+/**
+ * @brief Count of the number of allowed path prefixes.
+ */
 size_t path_validator_allowed_count(path_validator_t *ctx);
 
+/**
+ * @brief Returns an internal buffer with a list of allowed path prefixes.
+ *
+ * @details Formats a list of path prefixes into a user presentable string,
+ *   the function will truncate the string if the list of paths does not fit.
+ *   The buffer returned is owned by the path_validator object.
+ */
 const char* path_validator_base_paths(path_validator_t *ctx);
 
 #ifdef __cplusplus
