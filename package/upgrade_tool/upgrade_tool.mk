f1cb5756dce8408fde0e45c2bcb0c8b5e28746e5
Jason Mobarak <jason.mobarak@gmail.com>
2018-09-12 17:52:41 -0700

    Change to use GH releases

--- package/upgrade_tool/upgrade_tool.mk
+++ package/upgrade_tool/upgrade_tool.mk
@@ -5,7 +5,8 @@
 ################################################################################
 
 UPGRADE_TOOL_VERSION = v1.5.0-develop-2018091223
-UPGRADE_TOOL_SITE = $(call github,swift-nav,piksi_upgrade_tool_bin,$(UPGRADE_TOOL_VERSION))
+UPGRADE_TOOL_SOURCE = piksi_upgrade_tool.tgz
+UPGRADE_TOOL_SITE = https://github.com/swift-nav/piksi_upgrade_tool_bin/releases/download/$(UPGRADE_TOOL_VERSION)
 
 define UPGRADE_TOOL_INSTALL_TARGET_CMDS
 	$(MAKE) TARGET_DIR=$(TARGET_DIR) INSTALL=$(INSTALL) \
