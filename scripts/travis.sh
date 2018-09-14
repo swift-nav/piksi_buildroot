cab7eaf16e5c0eec97b9edfcb618c900d9564ded
Benjamin Altieri <benaltieri@swift-nav.com>
2018-09-14 16:09:29 -0700

    Hook format check into travis

--- scripts/travis.sh
+++ scripts/travis.sh
@@ -28,6 +28,8 @@ validate_travis_target()
     :
   elif [[ "${TRAVIS_TARGET}" == "nano" ]]; then
     :
+  elif [[ "${TRAVIS_TARGET}" == "format" ]]; then
+    :
   else
     echo "ERROR: unknown TRAVIS_TARGET value: ${TRAVIS_TARGET}" >&2
     exit 1
@@ -104,6 +106,18 @@ do_default_after_failure_actions()
 
   PRODUCT_VERSION=v3 PRODUCT_REV=prod \
     ./scripts/publish.sh $BUILD_LOG
+}
+
+check_format_errors() {
+  if [[ $(git --no-pager diff --name-only HEAD) ]]; then
+    echo "######################################################"
+    echo "####### clang-format warning found! Exiting... #######"
+    echo "######################################################"
+    echo ""
+    echo "This should be formatted locally and pushed again..."
+    git --no-pager diff
+    exit 1
+  fi
 }
 
 #######################################################################
@@ -311,6 +325,25 @@ handle_sdk_after_failure_phase()
 }
 
 #######################################################################
+# Format build variant ################################################
+#######################################################################
+
+handle_format_script_phase()
+{
+  make clang-format && check_format_errors
+}
+
+handle_format_after_success_phase()
+{
+  :
+}
+
+handle_format_after_failure_phase()
+{
+  do_default_after_failure_actions
+}
+
+#######################################################################
 # Travis build phase handling #########################################
 #######################################################################
 
@@ -328,6 +361,8 @@ handle_script_phase()
     handle_host_script_phase
   elif [[ "${TRAVIS_TARGET}" == "nano" ]]; then
     handle_nano_script_phase
+  elif [[ "${TRAVIS_TARGET}" == "format" ]]; then
+    handle_format_script_phase
   fi
 }
 
@@ -345,6 +380,8 @@ handle_after_success_phase()
     handle_host_after_success_phase
   elif [[ "${TRAVIS_TARGET}" == "nano" ]]; then
     handle_nano_after_success_phase
+  elif [[ "${TRAVIS_TARGET}" == "format" ]]; then
+    handle_format_after_success_phase
   fi
 }
 
@@ -362,6 +399,8 @@ handle_after_failure_phase()
     handle_host_after_failure_phase
   elif [[ "${TRAVIS_TARGET}" == "nano" ]]; then
     handle_nano_after_failure_phase
+  elif [[ "${TRAVIS_TARGET}" == "format" ]]; then
+    handle_format_after_failure_phase
   fi
 }
 
