fd5641d0dfb12841fd3a35d5da22b6c28f3085c4
Niilo Sirola <niilo.sirola@exafore.com>
2018-10-05 09:30:22 +0300

    Update libsbp to 2.4.5 (#903)
    * Update libsbp to 2.4.5
    
    * Update librtcm
    
    * update gnss-convertors

--- package/gnss_convertors/gnss_convertors.mk
+++ package/gnss_convertors/gnss_convertors.mk
@@ -4,7 +4,11 @@
 #
 ################################################################################
 
+<<<<<<< HEAD
 GNSS_CONVERTORS_VERSION = v0.3.61.6
+=======
+GNSS_CONVERTORS_VERSION = v0.3.65
+>>>>>>> 10fce301... Update libsbp to 2.4.1 (#903)
 GNSS_CONVERTORS_SITE = https://github.com/swift-nav/gnss-converters
 GNSS_CONVERTORS_SITE_METHOD = git
 GNSS_CONVERTORS_INSTALL_STAGING = YES
