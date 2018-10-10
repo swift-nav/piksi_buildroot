c5bee095658554b83642dd664abe3fa1444d0207
yiiiyang2010 <42586418+yiiiyang2010@users.noreply.github.com>
2018-10-09 20:40:10 -0700

    Fix memory error on libpiksi test [DEVC-1198] (#925)
    * Fix memory error in piksi_vlog
    
    * Add valgrind support to docker

--- package/libpiksi/libpiksi.mk
+++ package/libpiksi/libpiksi.mk
@@ -48,6 +48,7 @@ endif
 ifeq ($(BR2_RUN_TESTS),y)
 define LIBPIKSI_INSTALL_TARGET_CMDS_TESTS_RUN
 	sudo chroot $(TARGET_DIR) run_libpiksi_tests
+	#LD_LIBRARY_PATH=$(TARGET_DIR)/usr/lib valgrind --track-origins=yes --leak-check=full --error-exitcode=1 $(TARGET_DIR)/usr/bin/run_libpiksi_tests
 endef
 endif
 define LIBPIKSI_INSTALL_TARGET_CMDS
