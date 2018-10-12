a7d69fc16da2fe0ee98fdb5b4ae9fb3b47c2714a
Jason Mobarak <jason@swiftnav.com>
2018-10-12 14:11:45 -0700

    Hook up to remote branch for libsbp

--- package/libsbp/libsbp.mk
+++ package/libsbp/libsbp.mk
@@ -4,11 +4,11 @@
 #
 ################################################################################
 
-LIBSBP_VERSION = v2.4.1
-#LIBSBP_SITE = https://github.com/swift-nav/libsbp
-#LIBSBP_SITE_METHOD = git
-LIBSBP_SITE = /host/home/dev/libsbp
-LIBSBP_SITE_METHOD = local
+LIBSBP_VERSION = silverjam/resource_monitor
+LIBSBP_SITE = https://github.com/swift-nav/libsbp
+LIBSBP_SITE_METHOD = git
+#LIBSBP_SITE = /host/home/dev/libsbp
+#LIBSBP_SITE_METHOD = local
 LIBSBP_INSTALL_STAGING = YES
 LIBSBP_SUBDIR = c
 
