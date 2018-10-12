6f7ad04e720ac82acf03403daf516cdd7b1d19bf
Jason Mobarak <jason@swiftnav.com>
2018-10-12 00:16:29 -0700

    Register query interfaces

--- package/resource_monitor/src/resource_query.c
+++ package/resource_monitor/src/resource_query.c
@@ -12,12 +12,45 @@
 
 #include "resource_query.h"
 
+#include <sys/queue.h>
+
+typedef struct resq_node {
+  void* context;
+  resq_interface_t *query;
+  LIST_ENTRY(resq_node) entries;
+} resq_node_t;
+
+typedef LIST_HEAD(resq_nodes_head, resq_node) resq_node_head_t;
+
+resq_node_head_t interface_list = LIST_HEAD_INITIALIZER(resq_nodes_head);
+
 void resq_register(resq_interface_t *resq)
 {
-  /* TODO */
+  resq_node_t *node = malloc(sizeof(resq_node_t));
+
+  node->query = resq;
+  node->context = resq->init();
+
+  LIST_INSERT_HEAD(&interface_list, node, entries);
 }
 
-void resq_destory(resq_interface_t **resq)
+void resq_run_all(void) 
 {
-  /* TODO */
+  resq_node_t *node;
+
+  LIST_FOREACH(node, &interface_list, entries)
+  {
+    node->query->run_query(node->context);
+  }
+}
+
+void resq_destroy_all(void)
+{
+  resq_node_t *node = NULL;
+
+  while (!LIST_EMPTY(&interface_list)) {
+    node = LIST_FIRST(&interface_list);
+    LIST_REMOVE(node, entries);
+    free(node);
+  }
 }
