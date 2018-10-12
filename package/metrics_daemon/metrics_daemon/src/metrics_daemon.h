ef87318ad90e201c0dca20d48014f09de3824a49
Yi <yiiiyang2010@gmail.com>
2018-10-12 08:43:18 -0700

    Fix review comments

--- package/metrics_daemon/metrics_daemon/src/metrics_daemon.h
+++ package/metrics_daemon/metrics_daemon/src/metrics_daemon.h
@@ -1,7 +1,14 @@
-//
-// Created by Yi Yang on 9/7/18.
-//
-
+/*
+ * Copyright (C) 2018 Swift Navigation Inc.
+ * Contact: Swift Navigation <dev@swiftnav.com>
+ *
+ * This source is subject to the license found in the file 'LICENSE' which must
+ * be be distributed together with this source. All other rights reserved.
+ *
+ * THIS CODE AND INFORMATION IS PROVIDED "AS IS" WITHOUT WARRANTY OF ANY KIND,
+ * EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND/OR FITNESS FOR A PARTICULAR PURPOSE.
+ */
 #ifndef PIKSI_BUILDROOT_METRICS_DAEMON_H
 #define PIKSI_BUILDROOT_METRICS_DAEMON_H
 
