3236b4995cd0d36adbfbd48faacf0b0e9635a886
Niilo Sirola <niilo.sirola@exafore.com>
2018-10-19 14:49:07 +0300

    EX-388: RTCM output mode selection and MSM out (#926)
    * Update librtcm, libswiftnav and gnss_convertors
    
    * add RTCM output mode setting
    
    * Remove duplicate baseline_heading_callback

--- package/libswiftnav/libswiftnav.mk
+++ package/libswiftnav/libswiftnav.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-LIBSWIFTNAV_VERSION = v0.1.2
+LIBSWIFTNAV_VERSION = v0.1.3
 LIBSWIFTNAV_SITE = https://github.com/swift-nav/libswiftnav
 LIBSWIFTNAV_SITE_METHOD = git
 LIBSWIFTNAV_INSTALL_STAGING = YES
