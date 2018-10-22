7622f6ccbb4766eb84b20e355db2f5ad8c5f2e3f
Niilo Sirola <niilo.sirola@exafore.com>
2018-10-22 12:18:05 +0300

    Update gnss_convertors for NMEA empty timestamp fix

--- package/gnss_convertors/gnss_convertors.mk
+++ package/gnss_convertors/gnss_convertors.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-GNSS_CONVERTORS_VERSION = v0.3.67
+GNSS_CONVERTORS_VERSION = b3ed01d775c859d9755b0f0c904557567d1f2405
 GNSS_CONVERTORS_SITE = https://github.com/swift-nav/gnss-converters
 GNSS_CONVERTORS_SITE_METHOD = git
 GNSS_CONVERTORS_INSTALL_STAGING = YES
