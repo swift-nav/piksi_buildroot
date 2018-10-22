0b976780d595b1a9a5ca95fdda1a4e9969aa60b7
Dennis Zollo <dzollo@swift-nav.com>
2018-10-22 16:42:15 -0700

    Bump Smoothpose pointer

--- package/piksi_ins/piksi_ins.mk
+++ package/piksi_ins/piksi_ins.mk
@@ -17,7 +17,7 @@ endif # ! ($(BR2_BUILD_RELEASE_PROTECTED),y)
 
 $(info >>> Piksi INS is enabled, packaging with current image)
 
-PIKSI_INS_VERSION = v2.1.10 # Version 2.1.10 branch on October 10th, 2018
+PIKSI_INS_VERSION = v2.1.13 # Version 2.1.13 with initial velocity change
 
 PIKSI_INS_SITE = git@github.com:carnegieroboticsllc/piksi_ins.git
 PIKSI_INS_SITE_METHOD = git
