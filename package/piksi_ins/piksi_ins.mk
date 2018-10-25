adac8527d3c0555ca6b9437b758299a38ff2bc2b
Jason Mobarak <jason@swiftnav.com>
2018-10-25 11:25:09 -0700

    Make SmoothPose use a surpervisor, bump version (#965) [1/2]

--- package/piksi_ins/piksi_ins.mk
+++ package/piksi_ins/piksi_ins.mk
@@ -17,8 +17,7 @@ endif # ! ($(BR2_BUILD_RELEASE_PROTECTED),y)
 
 $(info >>> Piksi INS is enabled, packaging with current image)
 
-
-PIKSI_INS_VERSION = v2.1.10 # Version 2.1 branch on October 8th, 2018
+PIKSI_INS_VERSION = v2.1.15 # Version 2.1.15 pushed on Oct 25, 2018
 PIKSI_INS_SITE = git@github.com:carnegieroboticsllc/piksi_ins.git
 PIKSI_INS_SITE_METHOD = git
 PIKSI_INS_INSTALL_STAGING = YES
