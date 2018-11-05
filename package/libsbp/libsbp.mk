66d4405ccdcaa50f48a505fd71635cd25b08b504
Yi <yiiiyang2010@gmail.com>
2018-11-05 09:49:38 -0800

    add libsbp link

--- package/libsbp/libsbp.mk
+++ package/libsbp/libsbp.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-LIBSBP_VERSION = silverjam/resource_monitor
+LIBSBP_VERSION = 42dbbf0b12725c56abc7e1aeb7d9cf8c5a78a846
 LIBSBP_SITE = https://github.com/swift-nav/libsbp
 LIBSBP_SITE_METHOD = git
 #LIBSBP_SITE = /host/home/dev/libsbp
