48d7cf3bf29acba48ac181ea242261c66e02c143
Jason Mobarak <jason.mobarak@gmail.com>
2018-11-06 21:41:00 -0800

    Update endpoint_router.c

--- package/endpoint_router/endpoint_router/src/endpoint_router.c
+++ package/endpoint_router/endpoint_router/src/endpoint_router.c
@@ -2,7 +2,7 @@
  * Copyright (C) 2016-2018 Swift Navigation Inc.
  * Contact: Swift Navigation <dev@swiftnav.com>
  *
- * This source is subject to the license found tn the file 'LICENSE' which must
+ * This source is subject to the license found in the file 'LICENSE' which must
  * be be distributed together with this source. All other rights reserved.
  *
  * THIS CODE AND INFORMATION IS PROVIDED "AS IS" WITHOUT WARRANTY OF ANY KIND,
