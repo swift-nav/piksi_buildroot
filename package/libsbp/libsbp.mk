5e601089bf11d84da5a208e2bad26e4b1bc51eb0
Yi <yiiiyang2010@gmail.com>
2018-11-09 09:44:41 -0800

    updated libsbp hash to match the PR and fix a bug in querysockets.c

--- package/libsbp/libsbp.mk
+++ package/libsbp/libsbp.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-LIBSBP_VERSION = 42dbbf0b12725c56abc7e1aeb7d9cf8c5a78a846
+LIBSBP_VERSION = fdb525da507250981a5e1e1d6aed753dbc9f4120
 LIBSBP_SITE = https://github.com/swift-nav/libsbp
 LIBSBP_SITE_METHOD = git
 LIBSBP_INSTALL_STAGING = YES
