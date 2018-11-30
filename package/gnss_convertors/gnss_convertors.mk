9179347abb156feb17dad250a2cba63666c4a895
Jason Mobarak <jason@swiftnav.com>
2018-11-29 16:08:49 -0800

    Fix-up merge conflict

--- package/gnss_convertors/gnss_convertors.mk
+++ package/gnss_convertors/gnss_convertors.mk
@@ -4,11 +4,7 @@
 #
 ################################################################################
 
-<<<<<<< HEAD
 GNSS_CONVERTORS_VERSION = v0.3.61.6
-=======
-GNSS_CONVERTORS_VERSION = v0.3.65
->>>>>>> 10fce301... Update libsbp to 2.4.1 (#903)
 GNSS_CONVERTORS_SITE = https://github.com/swift-nav/gnss-converters
 GNSS_CONVERTORS_SITE_METHOD = git
 GNSS_CONVERTORS_INSTALL_STAGING = YES
