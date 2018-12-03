c91bdd107278e16afe64e5168e50bb3e580114c7
Dennis Zollo <dzollo@swift-nav.com>
2018-12-03 11:49:56 -0800

    update whitelist for CSAC_TELEMETRY messages

--- package/ports_daemon/ports_daemon/src/whitelists.c
+++ package/ports_daemon/ports_daemon/src/whitelists.c
@@ -74,7 +74,7 @@ static port_whitelist_config_t port_whitelist_config[PORT_MAX] = {
   },
   [PORT_UART1] = {
     .name = "uart1",
-    .wl = "23,65,72,74,81,97,117,134,136,137,138,139,144,149,163,165,166,167,171,175,181,185,187,188,189,190,257,258,259,520,522,524,526,527,528,1025,2305,2306,30583,65280,65282,65535"
+    .wl = "23,65,72,74,81,97,117,134,136,137,138,139,144,149,163,165,166,167,171,175,181,185,187,188,189,190,257,258,259,520,522,524,526,527,528,1025,2305,2306,30583,65280,65282,65284,65285,65535"
     /*  This filter represents the messages in use by the console.
         It removes all ECEF nav messages as well as parts of nav msg.
         MsgThreadState                23
@@ -119,6 +119,8 @@ static port_whitelist_config_t port_whitelist_config[PORT_MAX] = {
         MsgSbasRaw                 30583
         MsgStartup                 65280
         MsgDgnssStatus             65282
+        MsgCsacTelem               65284
+        MsgCsacTelemLabels         65285
         MsgHeartbeat               65535 */
   },
   [PORT_USB0] = {
@@ -127,7 +129,7 @@ static port_whitelist_config_t port_whitelist_config[PORT_MAX] = {
   },
   [PORT_TCP_SERVER0] = {
     .name = "tcp_server0",
-    .wl = "23,65,72,74,81,97,117,134,136,137,138,139,144,149,163,165,166,167,171,175,181,185,187,188,189,190,257,258,259,520,522,524,526,527,528,1025,2304,2305,2306,30583,65280,65282,65535"
+    .wl = "23,65,72,74,81,97,117,134,136,137,138,139,144,149,163,165,166,167,171,175,181,185,187,188,189,190,257,258,259,520,522,524,526,527,528,1025,2304,2305,2306,30583,65280,65282,65284,65285,65535"
     /*  This filter represents the messages in use by the console.
         It removes all ECEF nav messages as well as parts of nav msg.
         MsgThreadState                23
@@ -172,11 +174,13 @@ static port_whitelist_config_t port_whitelist_config[PORT_MAX] = {
         MsgSbasRaw                 30583
         MsgStartup                 65280
         MsgDgnssStatus             65282
+        MsgCsacTelem               65284
+        MsgCsacTelemLabels         65285
         MsgHeartbeat               65535 */
   },
   [PORT_TCP_SERVER1] = {
     .name = "tcp_server1",
-    .wl = "23,65,72,74,81,97,117,134,136,137,138,139,144,149,163,165,166,167,171,175,181,185,187,188,189,190,257,258,259,520,522,524,526,527,528,1025,2304,2305,2306,30583,65280,65282,65535"
+    .wl = "23,65,72,74,81,97,117,134,136,137,138,139,144,149,163,165,166,167,171,175,181,185,187,188,189,190,257,258,259,520,522,524,526,527,528,1025,2304,2305,2306,30583,65280,65282,65284,65285,65535"
     /*  This filter represents the messages in use by the console.
         It removes all ECEF nav messages as well as parts of nav msg.
         MsgThreadState                23
@@ -221,11 +225,13 @@ static port_whitelist_config_t port_whitelist_config[PORT_MAX] = {
         MsgSbasRaw                 30583
         MsgStartup                 65280
         MsgDgnssStatus             65282
+        MsgCsacTelem               65284
+        MsgCsacTelemLabels         65285
         MsgHeartbeat               65535 */
   },
   [PORT_TCP_CLIENT0] = {
     .name = "tcp_client0",
-    .wl = "23,65,72,74,81,97,117,134,136,137,138,139,144,149,163,165,166,167,171,175,181,185,187,188,189,190,257,258,259,520,522,524,526,527,528,1025,2304,2305,2306,30583,65280,65282,65535"
+    .wl = "23,65,72,74,81,97,117,134,136,137,138,139,144,149,163,165,166,167,171,175,181,185,187,188,189,190,257,258,259,520,522,524,526,527,528,1025,2304,2305,2306,30583,65280,65282,65284,65285,65535"
     /*  This filter represents the messages in use by the console.
         It removes all ECEF nav messages as well as parts of nav msg.
         MsgThreadState                23
@@ -270,11 +276,13 @@ static port_whitelist_config_t port_whitelist_config[PORT_MAX] = {
         MsgSbasRaw                 30583
         MsgStartup                 65280
         MsgDgnssStatus             65282
+        MsgCsacTelem               65284
+        MsgCsacTelemLabels         65285
         MsgHeartbeat               65535 */
   },
   [PORT_TCP_CLIENT1] = {
     .name = "tcp_client1",
-    .wl = "23,65,72,74,81,97,117,134,136,137,138,139,144,149,163,165,166,167,171,175,181,185,187,188,189,190,257,258,259,520,522,524,526,527,528,1025,2304,2305,2306,30583,65280,65282,65535"
+    .wl = "23,65,72,74,81,97,117,134,136,137,138,139,144,149,163,165,166,167,171,175,181,185,187,188,189,190,257,258,259,520,522,524,526,527,528,1025,2304,2305,2306,30583,65280,65282,65284,65285,65535"
     /*  This filter represents the messages in use by the console.
         It removes all ECEF nav messages as well as parts of nav msg.
         MsgThreadState                23
@@ -319,6 +327,8 @@ static port_whitelist_config_t port_whitelist_config[PORT_MAX] = {
         MsgSbasRaw                 30583
         MsgStartup                 65280
         MsgDgnssStatus             65282
+        MsgCsacTelem               65284
+        MsgCsacTelemLabels         65285
         MsgHeartbeat               65535 */
   },
   [PORT_UDP_SERVER0] = {
@@ -331,7 +341,7 @@ static port_whitelist_config_t port_whitelist_config[PORT_MAX] = {
   },
   [PORT_UDP_CLIENT0] = {
     .name = "udp_client0",
-    .wl = "23,65,72,74,81,97,117,134,136,137,138,139,144,149,163,165,166,167,171,175,181,185,187,188,189,190,257,258,259,520,522,524,526,527,528,1025,2304,2305,2306,30583,65280,65282,65535"
+    .wl = "23,65,72,74,81,97,117,134,136,137,138,139,144,149,163,165,166,167,171,175,181,185,187,188,189,190,257,258,259,520,522,524,526,527,528,1025,2304,2305,2306,30583,65280,65282,65284,65285,65535"
     /*  This filter represents the messages in use by the console.
         It removes all ECEF nav messages as well as parts of nav msg.
         MsgThreadState                23
@@ -376,11 +386,13 @@ static port_whitelist_config_t port_whitelist_config[PORT_MAX] = {
         MsgSbasRaw                 30583
         MsgStartup                 65280
         MsgDgnssStatus             65282
+        MsgCsacTelem               65284
+        MsgCsacTelemLabels         65285
         MsgHeartbeat               65535 */
   },
   [PORT_UDP_CLIENT1] = {
     .name = "udp_client1",
-    .wl = "23,65,72,74,81,97,117,134,136,137,138,139,144,149,163,165,166,167,171,175,181,185,187,188,189,190,257,258,259,520,522,524,526,527,528,1025,2304,2305,2306,30583,65280,65282,65535"
+    .wl = "23,65,72,74,81,97,117,134,136,137,138,139,144,149,163,165,166,167,171,175,181,185,187,188,189,190,257,258,259,520,522,524,526,527,528,1025,2304,2305,2306,30583,65280,65282,65284,65285,65535"
     /*  This filter represents the messages in use by the console.
         It removes all ECEF nav messages as well as parts of nav msg.
         MsgThreadState                23
@@ -425,6 +437,8 @@ static port_whitelist_config_t port_whitelist_config[PORT_MAX] = {
         MsgSbasRaw                 30583
         MsgStartup                 65280
         MsgDgnssStatus             65282
+        MsgCsacTelem               65284
+        MsgCsacTelemLabels         65285
         MsgHeartbeat               65535 */
   }
 };
