f1b53502cd44bdf36651d7d190456dd80f7dd093
Dennis Zollo <dzollo@swift-nav.com>
2018-12-04 13:24:51 -0800

    csac_daemon: port to master

--- package/csac_daemon/csac_daemon.mk
+++ package/csac_daemon/csac_daemon.mk
@@ -8,7 +8,7 @@ CSAC_DAEMON_VERSION = 0.1
 CSAC_DAEMON_SITE = \
   "${BR2_EXTERNAL_piksi_buildroot_PATH}/package/csac_daemon"
 CSAC_DAEMON_SITE_METHOD = local
-CSAC_DAEMON_DEPENDENCIES = libuv nanomsg_custom libsbp libpiksi
+CSAC_DAEMON_DEPENDENCIES = libuv libsbp libpiksi libsettings
 
 define CSAC_DAEMON_BUILD_CMDS
 	$(MAKE) CC=$(TARGET_CC) LD=$(TARGET_LD) -C $(@D)/src all
