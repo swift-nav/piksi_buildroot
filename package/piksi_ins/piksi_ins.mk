c01c309f595e8b5e9d7aa913c27210c688f7624c
Jason Mobarak <jason.mobarak@gmail.com>
2018-12-05 14:26:50 -0800

    Bump to piksi_ins updated for master

--- package/piksi_ins/piksi_ins.mk
+++ package/piksi_ins/piksi_ins.mk
@@ -17,7 +17,7 @@ endif # ! ($(BR2_BUILD_RELEASE_PROTECTED),y)
 
 $(info >>> Piksi INS is enabled, packaging with current image)
 
-PIKSI_INS_VERSION = v2.2.0 # Version 2.2.0 pushed on Nov 14, 2018
+PIKSI_INS_VERSION = 6f30eb3ae9e475383bf01b1c09910c190c48f782
 PIKSI_INS_SITE = git@github.com:carnegieroboticsllc/piksi_ins.git
 PIKSI_INS_SITE_METHOD = git
 PIKSI_INS_INSTALL_STAGING = YES
