8c857f1fd1d1c177b145ad95a9f92facee102471
Swift Svc Jenkins <jenkins@swift-nav.com>
2018-12-06 02:44:02 +0000

    Release Tag v2.2.6

--- fetch_firmware.sh
+++ fetch_firmware.sh
@@ -28,8 +28,8 @@ if [[ $(uname -a) == *NixOS* ]]; then
 fi
 
 BR_VERSION=$(git describe --abbrev=0 --tags)
-FW_VERSION=${1:-v2.2.5}
-NAP_VERSION=${2:-v2.2.5}
+FW_VERSION=${1:-v2.2.6}
+NAP_VERSION=${2:-v2.2.6}
 
 CCACHE_S3_PATH=s3://swiftnav-artifacts/piksi_buildroot/$BR_VERSION
 FW_S3_PATH=s3://swiftnav-releases/piksi_firmware_private/$FW_VERSION/v3
