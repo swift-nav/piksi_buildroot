19f1d4d5ea0b35626c60037807f3c6c471642f17
Pasi Miettinen <pmiettinen@exafore.com>
2019-01-07 14:12:22 +0200

    Update to libsettings-0.1.3 and build only libsettings.so

--- package/libsettings/libsettings.mk
+++ package/libsettings/libsettings.mk
@@ -4,10 +4,14 @@
 #
 ################################################################################
 
-LIBSETTINGS_VERSION = v0.1.2
+LIBSETTINGS_VERSION = v0.1.3
 LIBSETTINGS_SITE = https://github.com/swift-nav/libsettings
 LIBSETTINGS_SITE_METHOD = git
 LIBSETTINGS_INSTALL_STAGING = YES
 LIBSETTINGS_DEPENDENCIES = libsbp
+
+define LIBSETTINGS_BUILD_CMDS
+    $(MAKE) -C $(@D) settings
+endef
 
 $(eval $(cmake-package))
