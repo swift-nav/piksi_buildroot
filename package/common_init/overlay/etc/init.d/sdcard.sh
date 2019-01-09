5816046409f5a2dd0b005bdeaad4953c89ed078e
Jason Mobarak <jason.mobarak@gmail.com>
2019-01-08 18:51:37 -0800

    Don't have this service run forever
    Prevent the migrate_sdcard service from running forever if the board
    doesn't have an sdcard attached.
    
    Also use query_config tool instead of manually parsing config.ini.

--- package/common_init/overlay/etc/init.d/sdcard.sh
+++ package/common_init/overlay/etc/init.d/sdcard.sh
@@ -2,6 +2,9 @@ MOUNT_BASE="/media"
 MOUNTNAME="mmcblk0p1"
 MOUNTPOINT="$MOUNT_BASE/$MOUNTNAME"
 
+# Wait 30 seconds for the sdcard to be mounted
+wait_sdcard_limit=30
+
 is_mounted()
 {
   grep -q $MOUNTPOINT /proc/mounts
@@ -12,33 +15,63 @@ inotify_wait_mountpoint()
   [[ -e "$MOUNTPOINT" ]] || inotifywait -e create "$MOUNT_BASE" -q -t 1 | grep -q "CREATE $MOUNTNAME"
 }
 
+seconds_since_start() {
+  local the_time=$(cut -f1 -d' ' </proc/uptime)
+  echo ${the_time/.*}
+}
+
 wait_for_sdcard_mount()
 {
+  local start_time=$(seconds_since_start)
+  local current_time=
+
   while ! inotify_wait_mountpoint; do
+
+    current_time=$(seconds_since_start)
+    if [[ $(( current_time - start_time )) -gt $wait_sdcard_limit ]]; then
+      logi "Ending wait for sdcard mount..."
+      exit 0
+    fi
+
     continue;
   done
+
   while ! is_mounted; do
-    sleep 0.5; continue;
+
+    current_time=$(seconds_since_start)
+    if [[ $(( current_time - start_time )) -gt $wait_sdcard_limit ]]; then
+      logi "Ending wait for sdcard mount..."
+      exit 0
+    fi
+
+    sleep 0.5;
+    continue;
   done
+
   if ! [[ -d "$MOUNTPOINT" ]]; then
     logw "Mountpoint exists, but is not a directory"
   fi
+}
+
+is_f2fs_enabled()
+{
+  [[ "$(query_config --section standalone_logging --key logging_file_system)" == "F2FS" ]]
 }
 
 needs_migration()
 {
   local devname=$1; shift
 
-  if ! grep -q "logging_file_system=F2FS" /persistent/config.ini; then
+  if ! is_f2fs_enabled; then
     logd "Did not detect F2FS in config"
-    return 1
+    return 1 # no
   fi
 
   if detect_f2fs $devname; then
-    return 1
+    return 1 # no
   fi
 
-  return 0
+  return 0 # yes
 }
 
 detect_f2fs()
