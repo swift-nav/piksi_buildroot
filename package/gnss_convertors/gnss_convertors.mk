70de6c7f6fe07ade33f85c1acf7e127c0208284d
Niilo Sirola <niilo.sirola@exafore.com>
2019-01-14 15:26:24 +0200

    gnss-convertors to temp branch

--- package/gnss_convertors/gnss_convertors.mk
+++ package/gnss_convertors/gnss_convertors.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-GNSS_CONVERTORS_VERSION = v0.3.72
+GNSS_CONVERTORS_VERSION = 9a537c1c245ab7917f8d8e2cb5d8ad776eeb3151
 GNSS_CONVERTORS_SITE = https://github.com/swift-nav/gnss-converters
 GNSS_CONVERTORS_SITE_METHOD = git
 GNSS_CONVERTORS_INSTALL_STAGING = YES
