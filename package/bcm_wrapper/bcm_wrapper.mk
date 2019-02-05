47d36297686d1982ae2bf93456f0f0faf9a13be4
Gareth McMullin <gareth@blacksphere.co.nz>
2019-02-05 11:34:17 -0800

    BCM+Starling EVK (Nano) package updates (#1085)
    * Map UART devs to Zynq naming for ports daemon
    
    * Add basic bcm_wrapper package
    
    * Move grpc patches to package directory
    
    * Don't install ota_daemon startup script if disabled.
    
    * Update nano defconfig
    
    * Add Starling for Nano
    
    * Add script to generate uuid if needed

---
+++ package/bcm_wrapper/bcm_wrapper.mk
@@ -0,0 +1,39 @@
+################################################################################
+#
+# bcm_wrapper
+#
+################################################################################
+
+ifeq ($(BR2_PACKAGE_BCM_WRAPPER),y)
+
+BCM_WRAPPER_VERSION = v0.3.26
+BCM_WRAPPER_PREFIX = bcm_wrapper
+BCM_WRAPPER_ASSET = bcm_wrapper.tar.gz
+BCM_WRAPPER_S3 = $(call pbr_s3_url,$(BCM_WRAPPER_PREFIX),$(BCM_WRAPPER_VERSION),$(BCM_WRAPPER_ASSET))
+BCM_WRAPPER_SOURCE = $(call pbr_s3_src,$(BCM_WRAPPER_PREFIX),$(BCM_WRAPPER_VERSION),$(BCM_WRAPPER_ASSET))
+
+BCM_WRAPPER_SITE = $(DL_DIR)
+BCM_WRAPPER_SITE_METHOD = file
+
+define BCM_WRAPPER_PRE_DOWNLOAD_FIXUP
+	$(call pbr_s3_cp,$(BCM_WRAPPER_S3),$(BCM_WRAPPER_SITE),$(BCM_WRAPPER_SOURCE))
+endef
+
+define BCM_WRAPPER_USERS
+	bcmd -1 bcmd -1 * - - -
+endef
+
+BCM_WRAPPER_PRE_DOWNLOAD_HOOKS += BCM_WRAPPER_PRE_DOWNLOAD_FIXUP
+
+define BCM_WRAPPER_INSTALL_TARGET_CMDS
+	( $(foreach l,$(BCM_WRAPPER_LIBS),$(INSTALL) $(@D)/$(strip $l) $(TARGET_DIR)/usr/lib/ &&) \
+		exit 0 || exit 1 )
+	$(INSTALL) $(@D)/bcm_wrapper $(TARGET_DIR)/usr/bin/bcm_wrapper
+endef
+
+BCM_WRAPPER_OVERLAY = "${BR2_EXTERNAL_piksi_buildroot_PATH}/package/bcm_wrapper/overlay"
+BR2_ROOTFS_OVERLAY += "${BCM_WRAPPER_OVERLAY}"
+
+$(eval $(generic-package))
+
+endif
