f755da2e789b687de7d4c5483f8878f8baa5c5df
Niilo Sirola <niilo.sirola@exafore.com>
2019-02-12 11:23:00 +0200

    gnss-converters test (NMEA GSA in Time Matched mode)

--- package/gnss_convertors/gnss_convertors.mk
+++ package/gnss_convertors/gnss_convertors.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-GNSS_CONVERTORS_VERSION = v0.3.75
+GNSS_CONVERTORS_VERSION = 0f5214390574da0181124298252a004298263518
 GNSS_CONVERTORS_SITE = https://github.com/swift-nav/gnss-converters
 GNSS_CONVERTORS_SITE_METHOD = git
 GNSS_CONVERTORS_INSTALL_STAGING = YES
