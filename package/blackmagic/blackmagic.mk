422b336c244e470d2cdfca34149f479b2102f5b7
Gareth McMullin <gareth@blacksphere.co.nz>
2019-02-15 14:31:57 +1300

    Update blackmagic for zynqmp_r5

--- package/blackmagic/blackmagic.mk
+++ package/blackmagic/blackmagic.mk
@@ -5,7 +5,7 @@
 ################################################################################
 
 BLACKMAGIC_SITE = https://github.com/swift-nav/blackmagic
-BLACKMAGIC_VERSION = 10667ec18937b39fdec0957cf8f222bfad3200ae
+BLACKMAGIC_VERSION = 7c1c7099881548cd96b5834ed0bbd3d9b0cab925
 BLACKMAGIC_SITE_METHOD = git
 
 define BLACKMAGIC_BUILD_CMDS
