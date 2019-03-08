12b7cc9791987ca704ae61f94c313f888bb28e19
Yizhe Liu <yizow@berkeley.edu>
2019-03-08 13:41:21 -0800

    enable overlay for j1939

--- package/j1939_protocol/j1939_protocol.mk
+++ package/j1939_protocol/j1939_protocol.mk
@@ -6,13 +6,13 @@
 
 J1939_PROTOCOL_VERSION = 0.1
 J1939_PROTOCOL_SITE = \
-  "${BR2_EXTERNAL_piksi_buildroot_PATH}/package/j1939_protocol/src"
+  "${BR2_EXTERNAL_piksi_buildroot_PATH}/package/j1939_protocol"
 J1939_PROTOCOL_SITE_METHOD = local
 J1939_PROTOCOL_DEPENDENCIES =
 J1939_PROTOCOL_INSTALL_STAGING = YES
 
 define J1939_PROTOCOL_BUILD_CMDS
-    $(MAKE) CC=$(TARGET_CC) LD=$(TARGET_LD) LTO_PLUGIN="$(LTO_PLUGIN)" -C $(@D) all
+    $(MAKE) CC=$(TARGET_CC) LD=$(TARGET_LD) LTO_PLUGIN="$(LTO_PLUGIN)" -C $(@D)/src all
 endef
 
 define J1939_PROTOCOL_INSTALL_STAGING_CMDS
@@ -25,7 +25,9 @@ define J1939_PROTOCOL_INSTALL_TARGET_CMDS
     $(INSTALL) -D -m 0755 $(@D)/libj1939_protocol.so*                         \
                           $(TARGET_DIR)/usr/lib/endpoint_protocols
     $(INSTALL) -d -m 0755 $(TARGET_DIR)/etc/endpoint_router
-    $(INSTALL) -D -m 0755 $(@D)/j1939_router.yml $(TARGET_DIR)/etc/endpoint_router
+    $(INSTALL) -D -m 0755 $(@D)/src/j1939_router.yml $(TARGET_DIR)/etc/endpoint_router
 endef
+
+BR2_ROOTFS_OVERLAY += "${J1939_PROTOCOL_SITE}/overlay"
 
 $(eval $(generic-package))
