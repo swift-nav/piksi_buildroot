34b18d00e26cd8b3bf0892f4d22a40692d979000
Yizhe Liu <yizow@berkeley.edu>
2019-03-15 14:58:19 -0700

    fix acceleration output

--- package/sbp_j1939_bridge/src/sbp_j1939_bridge.c
+++ package/sbp_j1939_bridge/src/sbp_j1939_bridge.c
@@ -85,7 +85,7 @@ static int handle_acceleration_sensor(const u8 *payload, const u8 payload_length
   }
 
   (void) payload_length;
-  piksi_log(LOG_ERR, "Acceleration X, Y, Z : %.2f, %.2f, %.2f", acceleration[0] - 320, acceleration[1] - 320, acceleration[2] - 320);
+  piksi_log(LOG_ERR, "Acceleration X, Y, Z : %.2f, %.2f, %.2f", (acceleration[0] - 32000) / 100.0, (acceleration[1] - 32000) / 100.0, (acceleration[2] - 32000) / 100.0);
   return 0;
 }
 
