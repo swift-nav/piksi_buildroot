b2a03fa1189281d4df81cad4ea8113cc501cb13d
Jason Mobarak <jason@swiftnav.com>
2019-03-18 10:33:02 -0700

    Update external.mk

--- external.mk
+++ external.mk
@@ -13,6 +13,9 @@ BR2_TARGET_OPTIMIZATION := $(filter-out,$(BR2_TARGET_OPTIMIZATION),-flto)
 BR2_TARGET_CFLAGS := $(filter-out,$(BR2_TARGET_CFLAGS),-flto)
 BR2_TARGET_LDFLAGS := $(filter-out,$(BR2_TARGET_LDFLAGS),-flto)
 
+force-uninstall-toolchain-wrappers:
+	@:
+
 else
 
 include $(BR2_EXTERNAL_piksi_buildroot_PATH)/scripts/lto.mk
