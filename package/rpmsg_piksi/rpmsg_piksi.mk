bc5137099568bddd88da153f3f6a16cff32c2a24
Matt Woodward <matthew.woodward@swift-nav.com>
2019-03-26 14:36:42 -0700

    Move rpmsg_piksi to external repo

--- package/rpmsg_piksi/rpmsg_piksi.mk
+++ package/rpmsg_piksi/rpmsg_piksi.mk
@@ -4,10 +4,9 @@
 #
 ################################################################################
 
-RPMSG_PIKSI_VERSION = 0.1
-RPMSG_PIKSI_SITE = \
-  "${BR2_EXTERNAL_piksi_buildroot_PATH}/package/rpmsg_piksi/src"
-RPMSG_PIKSI_SITE_METHOD = local
+RPMSG_PIKSI_VERSION = master
+RPMSG_PIKSI_SITE = git@github.com:woodfell/rpmsg_piksi.git
+RPMSG_PIKSI_SITE_METHOD = git
 
 $(eval $(kernel-module))
 $(eval $(generic-package))
