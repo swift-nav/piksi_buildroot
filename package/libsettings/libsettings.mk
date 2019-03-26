e2d1145ce94a5d7be2fab2477a6cd5f24eb2b962
Matt Woodward <matthew.woodward@swift-nav.com>
2019-03-26 14:27:40 -0700

    Fix dependencies

--- package/libsettings/libsettings.mk
+++ package/libsettings/libsettings.mk
@@ -8,7 +8,7 @@ LIBSETTINGS_VERSION = woodfell/standardise_cmake_dependencies
 LIBSETTINGS_SITE = https://github.com/swift-nav/libsettings
 LIBSETTINGS_SITE_METHOD = git
 LIBSETTINGS_INSTALL_STAGING = YES
-LIBSETTINGS_DEPENDENCIES = libsbp
+LIBSETTINGS_DEPENDENCIES = libsbp libswiftnav
 
 define LIBSETTINGS_BUILD_CMDS
     $(MAKE) -C $(@D) settings
