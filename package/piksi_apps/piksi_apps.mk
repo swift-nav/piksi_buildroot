38bddc4c82f6b485aeacc22cdbabf6fa21585054
Matt Woodward <matthew.woodward@swift-nav.com>
2019-03-26 13:35:34 -0700

    gnss_convertors -> gnss_converters

--- package/piksi_apps/piksi_apps.mk
+++ package/piksi_apps/piksi_apps.mk
@@ -8,6 +8,6 @@ PIKSI_APPS_VERSION = piksi_apps_import
 PIKSI_APPS_SITE = git@github.com:woodfell/piksi_apps.git
 PIKSI_APPS_SITE_METHOD = git
 PIKSI_APPS_INSTALL_STAGING = YES
-PIKSI_APPS_DEPENDENCIES = libsbp libsettings cmph gnss_convertors libswiftnav json-v libuc libyaml libcurl libsocketcan rtcm
+PIKSI_APPS_DEPENDENCIES = libsbp libsettings cmph gnss_converters libswiftnav json-v libuc libyaml libcurl libsocketcan rtcm
 
 $(eval $(cmake-package))
