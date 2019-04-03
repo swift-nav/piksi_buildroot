7ff1ba579053027abb1fe004e2a79c21679d9f82
Pasi Miettinen <pmiettinen@exafore.com>
2019-04-03 11:00:36 +0300

    Update libsettings v0.1.11

--- package/libsettings/libsettings.mk
+++ package/libsettings/libsettings.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-LIBSETTINGS_VERSION = pmiettinen/esd-1204-parallel-requests
+LIBSETTINGS_VERSION = v0.1.11
 LIBSETTINGS_SITE = https://github.com/swift-nav/libsettings
 LIBSETTINGS_SITE_METHOD = git
 LIBSETTINGS_INSTALL_STAGING = YES
