a0dd35889727604279980d08ad734566e1300ad5
Swift Svc Jenkins <jenkins@swift-nav.com>
2019-04-11 18:50:04 +0000

    Release Tag v2.3.4 #no_auto_pr

--- fetch_firmware.sh
+++ fetch_firmware.sh
@@ -28,8 +28,8 @@ if [[ $(uname -a) == *NixOS* ]]; then
 fi
 
 FW_BUCKET=swiftnav-releases
-FW_VERSION=${1:-v2.3.3}
-NAP_VERSION=${2:-v2.3.3}
+FW_VERSION=${1:-v2.3.4}
+NAP_VERSION=${2:-v2.3.4}
 
 FW_S3_PATH=s3://$FW_BUCKET/piksi_firmware_private/$FW_VERSION/v3
 NAP_S3_PATH=s3://swiftnav-releases/piksi_fpga/$NAP_VERSION
