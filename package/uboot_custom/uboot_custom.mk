eed548735a8973736b004cc8500e90e9463f9e3d
Benjamin Altieri <benaltieri@swift-nav.com>
2019-04-17 14:52:50 -0700

    Use uart1 enabled uboot output from kernel logging

--- package/uboot_custom/uboot_custom.mk
+++ package/uboot_custom/uboot_custom.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-UBOOT_CUSTOM_VERSION = 3434394ef8694a21f008ccea8479909ded24fb56
+UBOOT_CUSTOM_VERSION = 98631ead429e6bd8e8414746c0b67ad42abb9fdd
 UBOOT_CUSTOM_SITE = https://github.com/swift-nav/u-boot-xlnx.git
 UBOOT_CUSTOM_SITE_METHOD = git
 UBOOT_CUSTOM_DEPENDENCIES = host-dtc host-uboot-tools host-openssl
