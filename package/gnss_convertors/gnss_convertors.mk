f8ea64dfb0301b4cf8d978d72ef63e63cb1d298e
Niilo Sirola <niilo.sirola@exafore.com>
2019-04-29 12:24:09 +0300

    Update librtcm and gnss-convertors pointers

--- package/gnss_convertors/gnss_convertors.mk
+++ package/gnss_convertors/gnss_convertors.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-GNSS_CONVERTORS_VERSION = 27d542b1080d81624bf87cdcd5bb6fb295f1e8cc
+GNSS_CONVERTORS_VERSION = v0.3.82
 GNSS_CONVERTORS_SITE = https://github.com/swift-nav/gnss-converters
 GNSS_CONVERTORS_SITE_METHOD = git
 GNSS_CONVERTORS_INSTALL_STAGING = YES
