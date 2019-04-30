8c865a7156c67d9cdd0bae0165cc76b1e1f340be
Jason Mobarak <jason.mobarak@gmail.com>
2019-04-30 12:23:51 -0700

    scripts: build variant support infra [ESD-1085] [2/7]
    Infrastructure for consuming the `build-variants.yaml` and
    `external-artifacts.yaml` config files.
    
    - `fetch-external-artifacts`: replaces the `fetch_firmware.sh` script
    and uses the `external-artifacts.yaml` and `build-variant.yaml` file
    to drive artifact fetching.
    
    - `gen-docker-sync`: generates a docker-sync config based on the
    DOCKER_HOST variant and the data in `build-variant.yaml`.
    
    - `gen-requirements-yaml`: generates a `requiment.yaml` config for
    consumption by HITL
    
    - `gen-variant-configs`: a very simple script that takes in config
    fragments listed in `build-variants.yaml` and dumps them into an
    output file specified by the `config_output` config field.
    
    - `pbr_script_lib.py`: library for code sharing between Python scripts
    
    - `run-preflight-checks`: supports the `build-variants.yaml` concept of
    "pre-flight" checks, which simply runs a set of scripts that verify the
    build environment prior to launching a build
    
    - `verify-aws-access`: makes sure AWS access is correctly configured
    (this check is "smart", in that it won't actually check if the file
    is already downloaded and the SHA256 matches the manifest).
    
    - `verify-generated-configs`: makes sure that the configs in
    `configs/*_defconfig` match their "generated" sources.
    
    - `verify-piksi-ins-axx`: make sure that access to Piksi INS is
    available if the variant is attempting to build INS support.
    
    - `yaml2json`: support binaries for converting yaml to json script
    Python does not have built-in support for YAML.

--- scripts/pbr_script_lib.py
+++ scripts/pbr_script_lib.py
@@ -15,10 +15,21 @@
 # environmental variables.
 
 import os
+import os.path as OP
 import sys
 import subprocess
+try:
+    import urlparse
+except ImportError:
+    import urllib.parse as urlparse
 import urllib
-import urlparse
+import platform
+from functools import wraps
+import hashlib
+
+PREFLIGHT_CONTEXT_FILE = '.preflight_context.pickle'
+_PREFLIGHT_CONTEXT = None
+
 
 class Runner(object):
     """
@@ -90,11 +101,14 @@ class Runner(object):
         ret = proc.wait()
         return (ret, proc.stdout.read().decode('utf8'), proc.stderr.read().decode('utf8'))
 
-    def check(self):
+    def check(self, stdout=None):
         """
         Run a pipeline and check that it's successful.
         """
-        proc = self._make_proc(stderr=subprocess.PIPE)
+        if stdout is None:
+            proc = self._make_proc(stderr=subprocess.PIPE)
+        else:
+            proc = self._make_proc(stdout=stdout, stderr=subprocess.PIPE)
         ret = proc.wait()
         if ret != 0:
             sys.stdout.write('\n')
@@ -102,16 +116,18 @@ class Runner(object):
                 "Process signaled error (exit code {}), standard error:\n\n{}"
                 .format(ret, proc.stderr.read().decode('utf8')))
 
+
 def find_dot_git():
-    curdir = os.getcwd() 
+    curdir = os.getcwd()
     try:
         while os.getcwd() != '/':
-            if os.path.exists('.git'):
+            if os.path.exists('.git') and os.path.isdir('.git'):
                 return os.getcwd()
             os.chdir('..')
     finally:
         os.chdir(curdir)
     return None
+
 
 # Adapted from https://stackoverflow.com/a/44552191/749342
 def build_url(baseurl, path, query_args):
@@ -120,5 +136,158 @@ def build_url(baseurl, path, query_args):
     url_parts[NETLOC_INDEX] = urlparse.urljoin(url_parts[NETLOC_INDEX], path)
     url_parts[QUERY_INDEX]= urllib.urlencode(query_args)
     return urlparse.urlunparse(url_parts)
+
+
+def cd_to_git_root(func):
+    @wraps(func)
+    def wrapper(*args, **kwds):
+        old_cwd = os.getcwd()
+        try:
+            return func(*args, **kwds)
+        finally:
+            os.chdir(old_cwd)
+    return wrapper
+
+
+@cd_to_git_root
+def clear_preflight_context():
+    if os.path.exists(PREFLIGHT_CONTEXT_FILE):
+        os.unlink(PREFLIGHT_CONTEXT_FILE)
+
+
+@cd_to_git_root
+def load_preflight_context():
+    global _PREFLIGHT_CONTEXT
+    if not os.path.exists(PREFLIGHT_CONTEXT_FILE):
+        _PREFLIGHT_CONTEXT = {}
+        return
+    import pickle
+    _PREFLIGHT_CONTEXT = pickle.load(open(PREFLIGHT_CONTEXT_FILE, 'rb'))
+
+
+def get_preflight_context():
+    if _PREFLIGHT_CONTEXT is None:
+        load_preflight_context()
+        import atexit
+        atexit.register(save_preflight_context)
+    return _PREFLIGHT_CONTEXT
+
+
+@cd_to_git_root
+def save_preflight_context():
+    import pickle
+    pickle.dump(open('.build_context.pickle', 'wb'), _PREFLIGHT_CONTEXT)
+
+
+@cd_to_git_root
+def list_build_variants():
+    build_variants = yaml2json('build-variants.yaml')
+    for variant in build_variants['variants']:
+        variant = variant['variant']
+        yield variant
+
+
+def find_build_variant(target_variant):
+    for variant in list_build_variants():
+        if variant['variant_name'] == target_variant:
+            return variant
+
+
+def _get_prop_with_transform(thing, prop_name, **additional_kwargs):
+    kwargs = dict(thing)
+    kwargs.update(additional_kwargs)
+    prop_val = thing.get(prop_name, '')
+    if isinstance(prop_val, str):
+        return prop_val.format(**kwargs)
+    return prop_val
+
+
+def get_artifact_prop(artifact, prop_name, **additional_kwargs):
+    return _get_prop_with_transform(artifact, prop_name, **additional_kwargs)
+
+
+def get_variant_prop(variant, prop_name, **additional_kwargs):
+    return _get_prop_with_transform(variant, prop_name, **additional_kwargs)
+
+
+def get_named_artifact(artifact_set_name, artifact_name):
+    artifacts = get_artifact_set(artifact_set_name)
+    for artifact in artifacts:
+        if artifact.get('name', '') == artifact_name:
+            return artifact
+    raise KeyError("Named artifact not found: {}".format(artifact_name))
+
+
+def get_artifact_set(name):
+    def transform(artifact):
+        kwargs = dict(artifact)
+        br2_dl_dir = os.environ.get('BR2_DL_DIR', 'buildroot/dl')
+        kwargs['BR2_DL_DIR'] = br2_dl_dir
+        return {K: V.format(**kwargs) for (K, V) in artifact.items()}
+    artifacts = yaml2json('external-artifacts.yaml')
+    for artifact_set in artifacts['artifact_sets']:
+        artifact_set = artifact_set['artifact_set']
+        if artifact_set['name'] == name:
+            merge_with = []
+            if 'merge_with' in artifact_set:
+                for mw_set in artifact_set['merge_with']:
+                    merge_with.extend(get_artifact_set(mw_set))
+            artifacts = artifact_set['artifacts']
+            return [transform(V) for V in artifacts + merge_with]
+    raise KeyError('could not find artifact set named: {}'.format(name))
+
+
+def build_s3_artifact_url(artifact):
+    s3_bucket = get_artifact_prop(artifact, 's3_bucket')
+    s3_repo = get_artifact_prop(artifact, 's3_repository')
+    version = get_artifact_prop(artifact, 'version')
+    s3_object = get_artifact_prop(artifact, 's3_object')
+    return 's3://{s3_bucket}/{s3_repo}/{version}/{s3_object}'.format(
+        s3_bucket=s3_bucket,
+        s3_repo=s3_repo,
+        version=version,
+        s3_object=s3_object)
+
+
+def get_config_hash_path(config_output):
+    dirname, basename = os.path.split(config_output)
+    hashdest = os.path.join(dirname, "hashes")
+    if not os.path.exists(hashdest):
+        os.mkdir(hashdest)
+    return os.path.join(hashdest, basename)
+
+
+def yaml2json_filename():
+    if platform.machine() == 'x86_64':
+        arch = 'amd64'
+    else:
+        arch = '386'
+    if platform.system() == 'Darwin':
+        system = 'darwin'
+    else:
+        system = 'linux'
+    bin_name = 'yaml2json_{}_{}'.format(system, arch)
+    path = OP.join(OP.dirname(OP.realpath(__file__)),
+                   'yaml2json.bin', bin_name)
+    return path
+
+
+def yaml2json(filename):
+    """
+    Converts yaml to json using the vendored yaml2json tool.
+    """
+    import json
+    yaml2json_tool = yaml2json_filename()
+    data = Runner().cat(filename).pipe().call(yaml2json_tool).to_str()
+    return json.loads(data)
+
+
+def sha256digest(filename):
+    sha256 = hashlib.new('sha256')
+    with open(filename, 'rb') as fp_input:
+        sha256.update(fp_input.read())
+    digest = sha256.hexdigest()
+    return digest
+
 
 # vim: ft=python:et:sts=4:ts=4:sw=4:tw=120:
