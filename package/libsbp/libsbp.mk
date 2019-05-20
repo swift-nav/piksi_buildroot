6a6518deca90a77608f20719cdc67ad75ca147a0
Niilo Sirola <niilo.sirola@exafore.com>
2019-05-20 09:44:42 +0300

    Update gnss-convertors, librtcm and libsbp

--- package/libsbp/libsbp.mk
+++ package/libsbp/libsbp.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-LIBSBP_VERSION = fa37aa69be28f2b8df26288600decf96c980aff5
+LIBSBP_VERSION = v2.5.5
 LIBSBP_SITE = https://github.com/swift-nav/libsbp
 LIBSBP_SITE_METHOD = git
 LIBSBP_INSTALL_STAGING = YES
