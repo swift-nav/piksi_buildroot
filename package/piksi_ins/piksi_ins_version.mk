39cd0c0348194bc11fccf5f1815b2e93e4858463
Swift Svc Jenkins <jenkins@swift-nav.com>
2019-08-14 00:40:21 +0000

    Release Tag v2.3.0-develop-2019081400 #no_auto_pr

--- package/piksi_ins/piksi_ins_version.mk
+++ package/piksi_ins/piksi_ins_version.mk
@@ -1,4 +1,4 @@
 # The piksi-releases project is used to manage the piksi_ins version,
 #   any updates here will be overwritten the next time piksi-releases
 #   is run.
-PIKSI_RELEASES_INS_VERSION = v2.3.0-develop-2019081003
+PIKSI_RELEASES_INS_VERSION = v2.3.0-develop-2019081400
