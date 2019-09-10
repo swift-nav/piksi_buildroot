5f354010e04bf89a0e57ed084547f1d55f964865
Colin Beighley <colinbeighley@gmail.com>
2019-09-10 15:53:51 -0700

    Disable ethernet PHY in u-boot

--- package/uboot_custom/uboot_custom.mk
+++ package/uboot_custom/uboot_custom.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-UBOOT_CUSTOM_VERSION = 3434394ef8694a21f008ccea8479909ded24fb56
+UBOOT_CUSTOM_VERSION = 00ae34f6dabadea8c383388526ad8c78f61d1122
 UBOOT_CUSTOM_SITE = https://github.com/swift-nav/u-boot-xlnx.git
 UBOOT_CUSTOM_SITE_METHOD = git
 UBOOT_CUSTOM_DEPENDENCIES = host-dtc host-uboot-tools host-openssl
