77209ea6039d2b43367f3fb1cdedb3fac0b8e717
Colin Beighley <colinbeighley@gmail.com>
2019-09-17 11:54:24 -0700

    Update u-boot to 10mbit ethernet SHA

--- package/uboot_custom/uboot_custom.mk
+++ package/uboot_custom/uboot_custom.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-UBOOT_CUSTOM_VERSION = 3434394ef8694a21f008ccea8479909ded24fb56
+UBOOT_CUSTOM_VERSION = e2290d9ed855d5f23e31baae9f4debc3abd81a9b
 UBOOT_CUSTOM_SITE = https://github.com/swift-nav/u-boot-xlnx.git
 UBOOT_CUSTOM_SITE_METHOD = git
 UBOOT_CUSTOM_DEPENDENCIES = host-dtc host-uboot-tools host-openssl
