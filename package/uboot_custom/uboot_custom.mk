8bbb3c8181bb4ee83838c7a181352535537b5118
Colin Beighley <colinbeighley@gmail.com>
2019-09-20 13:50:10 -0700

    Update u-boot to remove seemingly unnecessary write to MAC Specific Control Register

--- package/uboot_custom/uboot_custom.mk
+++ package/uboot_custom/uboot_custom.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-UBOOT_CUSTOM_VERSION = 3434394ef8694a21f008ccea8479909ded24fb56
+UBOOT_CUSTOM_VERSION = a9fc69c78f2aa15b8148506f2162759ad9324f23
 UBOOT_CUSTOM_SITE = https://github.com/swift-nav/u-boot-xlnx.git
 UBOOT_CUSTOM_SITE_METHOD = git
 UBOOT_CUSTOM_DEPENDENCIES = host-dtc host-uboot-tools host-openssl
