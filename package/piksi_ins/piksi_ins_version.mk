32066f8dffc90878d7d423e750eee8718b9c8e8c
dzollo <dzollo@swift-nav.com>
2019-12-05 13:29:55 -0800

    Update piksi_ins for 10 second smoothpose init period

--- package/piksi_ins/piksi_ins_version.mk
+++ package/piksi_ins/piksi_ins_version.mk
@@ -1,4 +1,4 @@
 # The piksi-releases project is used to manage the piksi_ins version,
 #   any updates here will be overwritten the next time piksi-releases
 #   is run.
-PIKSI_RELEASES_INS_VERSION = v2.3.20
+PIKSI_RELEASES_INS_VERSION = dc5da8752ceab1899dadd1f3237c071480e3fe57 # dzollo/Canadian_rail_10_second_init
