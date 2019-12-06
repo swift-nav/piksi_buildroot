76b4fa9d58ff3b4a2bd50bae8c83f8cf2bb7ef28
dzollo <dzollo@swift-nav.com>
2019-12-05 17:42:47 -0800

    bump version for init fixups

--- package/piksi_ins/piksi_ins_version.mk
+++ package/piksi_ins/piksi_ins_version.mk
@@ -1,4 +1,4 @@
 # The piksi-releases project is used to manage the piksi_ins version,
 #   any updates here will be overwritten the next time piksi-releases
 #   is run.
-PIKSI_RELEASES_INS_VERSION = 0ee775df45aac818484d77cf07a0dec04ced8a16 #dollo/piksi_ins_tighten_convergence_threshhold
+PIKSI_RELEASES_INS_VERSION = 17f8876154c8ebbe4f6ec3978ef2f2a3fdcc9d29 #dollo/piksi_ins_tighten_convergence_threshhold
