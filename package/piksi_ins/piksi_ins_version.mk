6b85e64eee6db682e3d5221733b2d31492367745
dzollo <dzollo@swift-nav.com>
2019-12-05 16:02:36 -0800

    update piksi_ins to dzollo/pikis_ins_forward_gnss_when_initializing_or_converging

--- package/piksi_ins/piksi_ins_version.mk
+++ package/piksi_ins/piksi_ins_version.mk
@@ -1,4 +1,4 @@
 # The piksi-releases project is used to manage the piksi_ins version,
 #   any updates here will be overwritten the next time piksi-releases
 #   is run.
-PIKSI_RELEASES_INS_VERSION = dc5da8752ceab1899dadd1f3237c071480e3fe57 # dzollo/Canadian_rail_10_second_init
+PIKSI_RELEASES_INS_VERSION = 372164fe4f6afc59b259840b8c73d7e7c8f528e0 #dzollo/forward_gnss_when_initializing_or_converging
