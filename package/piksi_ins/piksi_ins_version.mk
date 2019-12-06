9c983f8bb866e3dd7541bbc10424e430ecb70367
dzollo <dzollo@swift-nav.com>
2019-12-05 16:07:24 -0800

    Update Piksi INS to tighten INS convergence Threshhold

--- package/piksi_ins/piksi_ins_version.mk
+++ package/piksi_ins/piksi_ins_version.mk
@@ -1,4 +1,4 @@
 # The piksi-releases project is used to manage the piksi_ins version,
 #   any updates here will be overwritten the next time piksi-releases
 #   is run.
-PIKSI_RELEASES_INS_VERSION = 372164fe4f6afc59b259840b8c73d7e7c8f528e0 #dzollo/forward_gnss_when_initializing_or_converging
+PIKSI_RELEASES_INS_VERSION = 0ee775df45aac818484d77cf07a0dec04ced8a16 #dollo/piksi_ins_tighten_convergence_threshhold
