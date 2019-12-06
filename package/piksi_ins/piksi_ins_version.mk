dd0811b4eee166cddb530469101bf06b5baaae94
dzollo <dzollo@swift-nav.com>
2019-12-06 09:11:30 -0800

    Bump version to do state estimate even when forwarding

--- package/piksi_ins/piksi_ins_version.mk
+++ package/piksi_ins/piksi_ins_version.mk
@@ -1,4 +1,4 @@
 # The piksi-releases project is used to manage the piksi_ins version,
 #   any updates here will be overwritten the next time piksi-releases
 #   is run.
-PIKSI_RELEASES_INS_VERSION = 17f8876154c8ebbe4f6ec3978ef2f2a3fdcc9d29 #dollo/piksi_ins_tighten_convergence_threshhold
+PIKSI_RELEASES_INS_VERSION = 16094ad1663d2fcdca8ec9a188df91df9f6672ce #dollo/piksi_ins_tighten_convergence_threshhold
