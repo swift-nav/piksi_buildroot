22e457d15b9f716c437accbe4d483dabe4a9a7af
dzollo <dzollo@swift-nav.com>
2019-12-05 17:46:12 -0800

    bump ins version for init fixups

--- package/piksi_ins/piksi_ins_version.mk
+++ package/piksi_ins/piksi_ins_version.mk
@@ -1,4 +1,4 @@
 # The piksi-releases project is used to manage the piksi_ins version,
 #   any updates here will be overwritten the next time piksi-releases
 #   is run.
-PIKSI_RELEASES_INS_VERSION = 372164fe4f6afc59b259840b8c73d7e7c8f528e0 #dzollo/forward_gnss_when_initializing_or_converging
+PIKSI_RELEASES_INS_VERSION = 8149ec1199957773957370b22a687b6349229704 #dzollo/forward_gnss_when_initializing_or_converging
