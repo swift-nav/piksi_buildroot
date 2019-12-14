aa2928c2cd7c5d8efe7deb856b2d1760b3dbbcd0
dzollo <dzollo@swift-nav.com>
2019-12-13 18:01:50 -0800

    use dzollo/Alignment_hysterisis tip

--- package/piksi_ins/piksi_ins_version.mk
+++ package/piksi_ins/piksi_ins_version.mk
@@ -1,4 +1,4 @@
 # The piksi-releases project is used to manage the piksi_ins version,
 #   any updates here will be overwritten the next time piksi-releases
 #   is run.
-PIKSI_RELEASES_INS_VERSION = 351bd2e822958358c680dcb85cf173b35848b20f
+PIKSI_RELEASES_INS_VERSION = 56c65e9d6df5812e96b95e127347c740d60ffb75
