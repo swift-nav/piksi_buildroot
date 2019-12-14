b6a6d2adb42208d6516f2a85ab9c4eb893bccbef
dzollo <dzollo@swift-nav.com>
2019-12-13 17:56:19 -0800

    Use piksi_ins from https://github.com/swift-nav/pose_daemon_wrapper/pull/64/commits

--- package/piksi_ins/piksi_ins_version.mk
+++ package/piksi_ins/piksi_ins_version.mk
@@ -1,4 +1,4 @@
 # The piksi-releases project is used to manage the piksi_ins version,
 #   any updates here will be overwritten the next time piksi-releases
 #   is run.
-PIKSI_RELEASES_INS_VERSION = 351bd2e822958358c680dcb85cf173b35848b20f
+PIKSI_RELEASES_INS_VERSION = af6f33c333fe218653fdb37a2fbbb73920b09293
