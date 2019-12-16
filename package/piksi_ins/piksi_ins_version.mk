dbfc8ca27d87948ad713be07228433900998f2cc
dzollo <dzollo@swift-nav.com>
2019-12-16 15:23:12 -0800

    fix

--- package/piksi_ins/piksi_ins_version.mk
+++ package/piksi_ins/piksi_ins_version.mk
@@ -1,4 +1,4 @@
 # The piksi-releases project is used to manage the piksi_ins version,
 #   any updates here will be overwritten the next time piksi-releases
 #   is run.
-PIKSI_RELEASES_INS_VERSION = af6f33c333fe218653fdb37a2fbbb73920b09293
+PIKSI_RELEASES_INS_VERSION = 49b6292573e14018352044f38c29f2077c2007ad
