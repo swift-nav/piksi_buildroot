dc3d324e40371b3f76962f6d7f2a38fa9de52ccc
dzollo <dzollo@swift-nav.com>
2020-01-13 09:24:47 -0800

    Update piksi_ins to tip of Stillness pf cfg update

--- package/piksi_ins/piksi_ins_version.mk
+++ package/piksi_ins/piksi_ins_version.mk
@@ -1,4 +1,4 @@
 # The piksi-releases project is used to manage the piksi_ins version,
 #   any updates here will be overwritten the next time piksi-releases
 #   is run.
-PIKSI_RELEASES_INS_VERSION =  d171d796925826b743c1818a8d81e301746bc466
+PIKSI_RELEASES_INS_VERSION = 187ae836266912ec65f00cc8388aa4c687c43b87
