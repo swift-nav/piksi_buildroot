38d060a6ab3dfe281b632a7cda79f4299bf6095d
dzollo <dzollo@swift-nav.com>
2020-01-13 16:11:52 -0800

    Update piksi_ins to https://github.com/swift-nav/pose_daemon_wrapper/pull/75/commits

--- package/piksi_ins/piksi_ins_version.mk
+++ package/piksi_ins/piksi_ins_version.mk
@@ -1,4 +1,4 @@
 # The piksi-releases project is used to manage the piksi_ins version,
 #   any updates here will be overwritten the next time piksi-releases
 #   is run.
-PIKSI_RELEASES_INS_VERSION = 187ae836266912ec65f00cc8388aa4c687c43b87
+PIKSI_RELEASES_INS_VERSION = bbacb98162ff7b04c53eb8d2cd6890163efb7588 # current tip of dzollo/stillness_pf_cfg_update
