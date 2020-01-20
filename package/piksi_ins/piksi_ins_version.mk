607fb5b236d7e8de5a92d65771832960bc5f40c6
denniszollo <dzollo@swift-nav.com>
2020-01-19 23:16:56 -0800

    Move piksi_ins to accel_noise config branch

--- package/piksi_ins/piksi_ins_version.mk
+++ package/piksi_ins/piksi_ins_version.mk
@@ -1,4 +1,4 @@
 # The piksi-releases project is used to manage the piksi_ins version,
 #   any updates here will be overwritten the next time piksi-releases
 #   is run.
-PIKSI_RELEASES_INS_VERSION =  f3f9ec2b1563a9e582f8efe4644eec46659b115b # output_lowpass
+PIKSI_RELEASES_INS_VERSION =  b81d246a4cb5903fa5ea34272b5c29680a8eb988 # accell noise config
