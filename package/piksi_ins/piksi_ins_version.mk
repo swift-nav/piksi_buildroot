6b0d8e1cfac41389399d14489f147d318b9a0308
denniszollo <dzollo@swift-nav.com>
2020-01-24 18:07:26 -0800

    Update piksi_ins_version.mk

--- package/piksi_ins/piksi_ins_version.mk
+++ package/piksi_ins/piksi_ins_version.mk
@@ -1,4 +1,4 @@
 # The piksi-releases project is used to manage the piksi_ins version,
 #   any updates here will be overwritten the next time piksi-releases
 #   is run.
-PIKSI_RELEASES_INS_VERSION =  b81d246a4cb5903fa5ea34272b5c29680a8eb988 # accell noise config
+PIKSI_RELEASES_INS_VERSION =  b0e8dccc8e5d17c78229746492128b89b60f1f1e # Lp filter and biquad filter
