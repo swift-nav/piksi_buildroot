9a4d4018a90ce440d916cfb78a0e8127e274b48f
dzollo <dzollo@swift-nav.com>
2020-02-14 15:48:11 -0800

    Update to tip of stillness branch with initialization check and work around

--- package/piksi_ins/piksi_ins_version.mk
+++ package/piksi_ins/piksi_ins_version.mk
@@ -1,4 +1,4 @@
 # The piksi-releases project is used to manage the piksi_ins version,
 #   any updates here will be overwritten the next time piksi-releases
 #   is run.
-PIKSI_RELEASES_INS_VERSION =  9c7269879f0997be0445e79ea0e50a7ba48ee6b3 # tip of v2.3.0-cn-stillness-hotfix 
+PIKSI_RELEASES_INS_VERSION = 79f3b0d80df08acc93ce74601737de3e834fbea9 # tip of v2.3.0-cn-stillness-hotfix as of 2020-02-14 
